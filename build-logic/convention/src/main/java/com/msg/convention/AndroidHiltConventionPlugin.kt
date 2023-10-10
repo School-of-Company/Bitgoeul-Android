d451121326955754a925cbe96fa0e86c370b1093
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-10 18:05:23 +0900

    :memo: create hilt plugin

---
+++ build-logic/convention/src/main/java/com/msg/convention/AndroidHiltConventionPlugin.kt
@@ -0,0 +1,26 @@
+package com.msg.convention
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.msg.convention.bitgoeul.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidHiltConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("dagger.hilt.android.plugin")
+                apply("org.jetbrains.kotlin.kapt")
+            }
+
+            dependencies {
+                add("implementation", (libs.findLibrary("hilt.android").get()))
+                add("kapt", (libs.findLibrary("hilt.compiler").get()))
+                add("kaptAndroidTest", (libs.findLibrary("hilt.compiler").get()))
+                add("kaptTest", (libs.findLibrary("hilt.compiler").get()))
+            }
+        }
+    }
+}
