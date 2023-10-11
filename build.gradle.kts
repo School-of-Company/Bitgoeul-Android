fa4f17d8706147a5fe7ccc14c01261ae959f7b99
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-11 11:52:23 +0900

    add core module

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
 }
