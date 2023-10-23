4180501d49aa80aa369b137bdca3bc11866836f8
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-23 11:59:37 +0900

    :memo: add Exception

---
+++ core/network/src/main/java/com/msg/network/exception/HttpException.kt
@@ -0,0 +1,42 @@
+package com.msg.network.exception
+
+class BadRequestException(
+    override val message: String?
+) : RuntimeException()
+
+class UnauthorizedException(
+    override val message: String?
+) : RuntimeException()
+
+class ForBiddenException(
+    override val message: String?
+) : RuntimeException()
+
+class NotFoundException(
+    override val message: String?
+) : RuntimeException()
+
+class NotAcceptableException(
+    override val message: String?
+) : RuntimeException()
+
+class ConflictException(
+    override val message: String?
+) : RuntimeException()
+
+class TimeOutException(
+    override val message: String?
+) : RuntimeException()
+
+class ServerException(
+    override val message: String?
+) : RuntimeException()
+
+class OtherException(
+    override val message: String?,
+    val code: Int
+) : RuntimeException()
+
+class UnknownException(
+    override val message: String?
+) : RuntimeException()
