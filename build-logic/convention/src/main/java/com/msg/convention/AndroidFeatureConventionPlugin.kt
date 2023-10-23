ca846ef5ae44e3be872a972768456734716b0a36
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-23 09:26:12 +0900

    :memo: add FeaturePlugin

---
+++ build-logic/convention/src/main/java/com/msg/convention/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,31 @@
+package com.msg.convention
+
+import com.msg.convention.bitgoeul.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("bitgoeul.android.core")
+                apply("bitgoeul.android.hilt")
+                apply("bitgoeul.android.compose")
+            }
+
+            dependencies {
+                add("implementation", project(":core:model"))
+                add("implementation", project(":core:ui"))
+                add("implementation", project(":core:design-system"))
+                add("implementation", project(":core:data"))
+                add("implementation", project(":core:domain"))
+
+                add("implementation", libs.findLibrary("coil.kt").get())
+
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+            }
+        }
+    }
+}
