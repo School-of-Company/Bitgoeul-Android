f52230906410f79af83f9ce300eba34df8cce037
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-25 10:10:37 +0900

    :memo: add signUpClubTeacher fun in AuthAPI

--- core/network/src/main/java/com/msg/network/api/AuthAPI.kt
+++ core/network/src/main/java/com/msg/network/api/AuthAPI.kt
@@ -2,6 +2,7 @@ package com.msg.network.api
 
 import com.msg.model.remote.AuthTokenModel
 import com.msg.model.remote.request.LoginRequest
+import com.msg.model.remote.request.SignUpJobClubTeacherRequest
 import com.msg.model.remote.request.SignUpStudentRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -15,5 +16,10 @@ interface AuthAPI {
     @POST("auth/student")
     suspend fun signUpStudent(
         @Body body: SignUpStudentRequest
+    )
+
+    @POST("auth/teacher")
+    suspend fun signUpJobClubTeacher(
+        @Body body: SignUpJobClubTeacherRequest
     )
 }
