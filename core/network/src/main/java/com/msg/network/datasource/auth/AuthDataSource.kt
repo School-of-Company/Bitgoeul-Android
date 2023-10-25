8e85fa668cf4a88d209069fc0dcc66d93e9c91dc
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-25 10:11:17 +0900

    :memo: add signUpJobClubTeacher fun in AuthDataSource

--- core/network/src/main/java/com/msg/network/datasource/auth/AuthDataSource.kt
+++ core/network/src/main/java/com/msg/network/datasource/auth/AuthDataSource.kt
@@ -2,10 +2,12 @@ package com.msg.network.datasource.auth
 
 import com.msg.model.remote.AuthTokenModel
 import com.msg.model.remote.request.LoginRequest
+import com.msg.model.remote.request.SignUpJobClubTeacherRequest
 import com.msg.model.remote.request.SignUpStudentRequest
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
     suspend fun login(body: LoginRequest): Flow<AuthTokenModel>
     suspend fun signUpStudent(body: SignUpStudentRequest): Flow<Unit>
+    suspend fun signUpJobClubTeacher(body: SignUpJobClubTeacherRequest): Flow<Unit>
 }
