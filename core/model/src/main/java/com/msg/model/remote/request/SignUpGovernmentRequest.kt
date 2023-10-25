ba3ffdd129ce281ca407f9900f4875a5acae344e
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-25 14:23:42 +0900

    :memo: add SignUpGovernmentRequest

---
+++ core/model/src/main/java/com/msg/model/remote/request/SignUpGovernmentRequest.kt
@@ -0,0 +1,13 @@
+package com.msg.model.remote.request
+
+import com.msg.model.remote.HighSchool
+
+data class SignUpGovernmentRequest(
+    val email: String,
+    val name: String,
+    val phoneNumber: String,
+    val password: String,
+    val highSchool: HighSchool,
+    val clubName: String,
+    val governmentName: String
+)
