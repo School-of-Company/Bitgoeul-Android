5a9f96da10524a59bc6f16d6b452a3678a7596b3
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-10-30 15:17:27 +0900

    :lipstick: Change to see last digit of Password TextField

---
+++ core/design-system/src/main/java/com/msg/design_system/util/LastPasswordVisibleVisualTransformation.kt
@@ -0,0 +1,28 @@
+package com.msg.design_system.util
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+class LastPasswordVisibleVisualTransformation(
+    private val mask: Char = '\u2022',
+    private val isFocused: Boolean,
+    private val isChanged: Boolean
+) : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val recentIsFocusedState: Boolean = if (isChanged) isFocused else !isFocused
+        return TransformedText(
+            text = if (recentIsFocusedState) {
+                if (text.text.isNotEmpty()) {
+                    AnnotatedString(mask.toString().repeat(text.text.length - 1) + text.text.last())
+                } else {
+                    AnnotatedString(mask.toString().repeat(text.text.length))
+                }
+            } else {
+                AnnotatedString(mask.toString().repeat(text.text.length))
+            },
+            offsetMapping = OffsetMapping.Identity
+        )
+    }
+}
