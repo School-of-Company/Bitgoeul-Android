fc761df8032ba4f12505c93849719e74ad5e9a41
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-09 08:59:19 +0900

    :sparkles: :: LectureDataSourceImpl 클래스 작성
    :: LectureDataSourceImpl 클래스 작성

---
+++ core/network/src/main/java/com/msg/network/datasource/lecture/LectureDataSourceImpl.kt
@@ -0,0 +1,64 @@
+package com.msg.network.datasource.lecture
+
+import com.msg.model.remote.model.LectureListModel
+import com.msg.model.remote.request.lecture.OpenLectureRequest
+import com.msg.network.api.LectureAPI
+import com.msg.network.util.BitgoeulApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.util.UUID
+import javax.inject.Inject
+
+class LectureDataSourceImpl @Inject constructor(
+    private val lectureAPI: LectureAPI,
+) : LectureDataSource {
+    override suspend fun openLecture(body: OpenLectureRequest): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { lectureAPI.openLecture(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun getLectureList(): Flow<List<LectureListModel>> = flow {
+        emit(
+            BitgoeulApiHandler<List<LectureListModel>>()
+                .httpRequest { lectureAPI.getLectureList() }
+                .sendRequest()
+        )
+    }
+
+    override suspend fun getDetailLecture(id: UUID): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { lectureAPI.getDetailLecture(id = id) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun lectureApplication(id: UUID): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { lectureAPI.lectureApplication(id = id) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun approvePendingLecture(id: UUID): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { lectureAPI.approvePendingLecture(id = id) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun rejectPendingLecture(id: UUID): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { lectureAPI.rejectPendingLecture(id = id) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+}
