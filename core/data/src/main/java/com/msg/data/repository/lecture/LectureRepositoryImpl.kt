4214145de81c9bdcc57cf5a5c2ccbb2528d08ca7
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-09 09:13:15 +0900

    :sparkles: :: LectureRepositoryImpl 클래스 작성
    :: LectureRepositoryImpl 클래스 작성

---
+++ core/data/src/main/java/com/msg/data/repository/lecture/LectureRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.msg.data.repository.lecture
+
+import com.msg.model.remote.model.LectureListModel
+import com.msg.model.remote.request.lecture.OpenLectureRequest
+import com.msg.network.datasource.lecture.LectureDataSource
+import kotlinx.coroutines.flow.Flow
+import java.util.UUID
+import javax.inject.Inject
+
+class LectureRepositoryImpl @Inject constructor(
+    private val lectureDataSource: LectureDataSource,
+) : LectureRepository {
+    override suspend fun openLecture(body: OpenLectureRequest): Flow<Unit> {
+        return lectureDataSource.openLecture(
+            body = body
+        )
+    }
+
+    override suspend fun getLectureList(): Flow<List<LectureListModel>> {
+        return lectureDataSource.getLectureList()
+    }
+
+    override suspend fun getDetailLecture(id: UUID): Flow<Unit> {
+        return lectureDataSource.getDetailLecture(
+            id = id
+        )
+    }
+
+    override suspend fun lectureApplication(id: UUID): Flow<Unit> {
+        return lectureDataSource.lectureApplication(
+            id = id
+        )
+    }
+
+    override suspend fun approvePendingLecture(id: UUID): Flow<Unit> {
+        return lectureDataSource.approvePendingLecture(
+            id = id
+        )
+    }
+
+    override suspend fun rejectPendingLecture(id: UUID): Flow<Unit> {
+        return lectureDataSource.rejectPendingLecture(
+            id = id
+        )
+    }
+}
