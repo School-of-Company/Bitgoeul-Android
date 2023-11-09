45ea80443e55a224de1bee7591ce165cdb61b689
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-09 09:18:42 +0900

    :sparkles: :: RepositoryModule에 LectureRepositoryImpl 추가
    :: RepositoryModule에 LectureRepositoryImpl 추가

--- core/data/src/main/java/com/msg/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/msg/data/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.msg.data.di
 
 import com.msg.data.repository.auth.AuthRepository
 import com.msg.data.repository.auth.AuthRepositoryImpl
+import com.msg.data.repository.lecture.LectureRepository
+import com.msg.data.repository.lecture.LectureRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,9 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         authRepositoryImpl: AuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    abstract fun bindLectureRepository(
+        lectureRepositoryImpl: LectureRepositoryImpl
+    ): LectureRepository
 }
