6563bd5c35512cf8683477067ba7f1869d38111e
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-09 09:42:13 +0900

    :fire: :: NetworkModule 불필요한 import문 삭제
    :: NetworkModule 불필요한 import문 삭제

--- core/network/src/main/java/com/msg/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/msg/network/di/NetworkModule.kt
@@ -13,7 +13,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
