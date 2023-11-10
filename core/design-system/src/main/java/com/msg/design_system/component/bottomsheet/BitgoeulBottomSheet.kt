33dab2de63876328f78494c83ff79be29682f7b3
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-10 10:16:26 +0900

    :lipstick: update bottomSheet

--- core/design-system/src/main/java/com/msg/design_system/component/bottomsheet/BitgoeulBottomSheet.kt
+++ core/design-system/src/main/java/com/msg/design_system/component/bottomsheet/BitgoeulBottomSheet.kt
@@ -1,5 +1,7 @@
 package com.msg.design_system.component.bottomsheet
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,7 +41,7 @@ fun SelectorBottomSheet(
                 }
             }
             item {
-                lastItem()
+                Spacer(modifier = Modifier.height(72.dp))
             }
         }
     }
