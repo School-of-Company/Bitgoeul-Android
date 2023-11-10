48b9de06cabea43f950d2c26fc1e3f20d618ca2c
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-10 10:18:21 +0900

    :memo: change searchClubBySchool fun

--- feature/sign-up/src/main/java/com/msg/sign_up/util/searchClubBySchool.kt
+++ feature/sign-up/src/main/java/com/msg/sign_up/util/searchClubBySchool.kt
@@ -1,5 +1,6 @@
 package com.msg.sign_up.util
 
+import com.msg.model.remote.HighSchool
 import com.msg.model.ui.DHSofFSClubList
 import com.msg.model.ui.GATClubList
 import com.msg.model.ui.GETHSClubList
@@ -11,22 +12,11 @@ import com.msg.model.ui.JTHSClubList
 import com.msg.model.ui.KTHSClubList
 import com.msg.model.ui.SGCHSClubList
 import com.msg.model.ui.SSTHSClubList
+import com.msg.sign_up.data.HighSchoolList
 
-fun String.searchBySchool(): List<String> {
-    when (this) {
-        "광주공업고등학교" -> return GTHSClubList
-        "금파공업고등학교" -> return KTHSClubList
-        "전남공업고등학교" -> return JTHSClubList
-        "광주여자상업고등학교" -> return GGCHSClubList
-        "전남여자상업고등학교" -> return listOf()
-        "광주자연과학고등학교" -> return GNSHSClubList
-        "광주전자공업고등학교" -> return GETHSClubList
-        "동일미래과학고등학교" -> return DHSofFSClubList
-        "서진여자고등학교" -> return listOf()
-        "숭의과학기술학교" -> return SSTHSClubList
-        "송원여자상업고등학교" -> return SGCHSClubList
-        "광주자동화설비마이스터고등학교" -> return GATClubList
-        "광주소프트웨어마이스터고등학교" -> return GSMClubList
-        else -> return listOf()
+fun String.searchClubBySchool(): List<String> {
+    HighSchool.values().forEach {
+        if (this == it.school) return it.club else listOf("error")
     }
+    return listOf("error")
 }
