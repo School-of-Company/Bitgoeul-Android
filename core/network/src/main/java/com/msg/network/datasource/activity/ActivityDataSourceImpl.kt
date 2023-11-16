b6be2a047faca654f187b0b8f8bc3f4c23982a6c
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-16 09:38:05 +0900

    :memo: add addStudentActivityInfo fun in ActivityDataSource

--- core/network/src/main/java/com/msg/network/datasource/activity/ActivityDataSourceImpl.kt
+++ core/network/src/main/java/com/msg/network/datasource/activity/ActivityDataSourceImpl.kt
@@ -1,9 +1,22 @@
 package com.msg.network.datasource.activity
 
+import com.msg.model.remote.model.activity.StudentActivityModel
 import com.msg.network.api.ActivityAPI
+import com.msg.network.util.BitgoeulApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class ActivityDataSourceImpl @Inject constructor(
     private val activityAPI: ActivityAPI
 ) : ActivityDataSource {
+    override suspend fun addStudentActivityInfo(body: StudentActivityModel): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { activityAPI.addStudentActivityInfo(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
 }
