a0f5dd24eac27ab9fe645d5b1bde468d2de4e491
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-16 11:27:49 +0900

    :memo: add editStudentActivity fun in ActivityAPI

--- core/network/src/main/java/com/msg/network/api/ActivityAPI.kt
+++ core/network/src/main/java/com/msg/network/api/ActivityAPI.kt
@@ -2,11 +2,20 @@ package com.msg.network.api
 
 import com.msg.model.remote.model.activity.StudentActivityModel
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.UUID
 
 interface ActivityAPI {
     @POST("activity")
     suspend fun addStudentActivityInfo(
+        @Body body: StudentActivityModel
+    )
+
+    @PATCH("patch/{id}")
+    suspend fun editStudentActivityInfo(
+        @Path("id") id: UUID,
         @Body body: StudentActivityModel
     )
 }
