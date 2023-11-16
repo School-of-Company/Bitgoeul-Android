0a6fed4aa741f4deee98d6711b9e43bd3151b10d
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-16 09:32:38 +0900

    :memo: bind ActivityDataSource

--- core/network/src/main/java/com/msg/network/di/DataSourceModule.kt
+++ core/network/src/main/java/com/msg/network/di/DataSourceModule.kt
@@ -1,5 +1,7 @@
 package com.msg.network.di
 
+import com.msg.network.datasource.activity.ActivityDataSource
+import com.msg.network.datasource.activity.ActivityDataSourceImpl
 import com.msg.network.datasource.auth.AuthDataSource
 import com.msg.network.datasource.auth.AuthDataSourceImpl
 import com.msg.network.datasource.lecture.LectureDataSource
@@ -21,4 +23,9 @@ abstract class DataSourceModule {
     abstract fun bindLectureDataSource(
         lectureDataSourceImpl: LectureDataSourceImpl
     ): LectureDataSource
+
+    @Binds
+    abstract fun bindActivityDataSource(
+        activityDataSourceImpl: ActivityDataSourceImpl
+    ): ActivityDataSource
 }
