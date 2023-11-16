7fe61705e0c18294e1f60e055b615ba8d3759cdf
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-16 09:57:07 +0900

    :memo: add addStudentActivityInfo fun in ActivityRepository

--- core/data/src/main/java/com/msg/data/repository/activity/ActivityRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/activity/ActivityRepository.kt
@@ -1,4 +1,9 @@
 package com.msg.data.repository.activity
 
+import com.msg.model.remote.model.activity.StudentActivityModel
+import kotlinx.coroutines.flow.Flow
+
+
 interface ActivityRepository {
+    suspend fun addStudentActivityInfo(body: StudentActivityModel): Flow<Unit>
 }
--- core/data/src/main/java/com/msg/data/repository/activity/ActivityRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/activity/ActivityRepositoryImpl.kt
@@ -1,9 +1,17 @@
 package com.msg.data.repository.activity
 
+import com.msg.model.remote.model.activity.StudentActivityModel
 import com.msg.network.datasource.activity.ActivityDataSource
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ActivityRepositoryImpl @Inject constructor(
-    activityDataSource: ActivityDataSource
+    private val activityDataSource: ActivityDataSource
 ) : ActivityRepository {
+    override suspend fun addStudentActivityInfo(body: StudentActivityModel): Flow<Unit> {
+        return activityDataSource.addStudentActivityInfo(
+            body = body
+        )
+    }
+
 }
