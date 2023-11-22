0b18e6876acd94a0a0bb60cdb2d92f7bbcb1b22a
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-22 12:12:43 +0900

    :sparkles: add checkEmailRegex

---
+++ core/design-system/src/main/java/com/msg/design_system/util/checkRegex.kt
@@ -0,0 +1,6 @@
+package com.msg.design_system.util
+
+fun String.checkEmailRegex(): Boolean {
+    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
+    return this.matches(emailRegex.toRegex())
+}
