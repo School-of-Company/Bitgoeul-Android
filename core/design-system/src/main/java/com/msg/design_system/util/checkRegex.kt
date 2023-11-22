04f38e60a29dbf4051bff17727e9facca8a48c0c
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-11-22 12:39:41 +0900

    :sparkles: add checkPasswordRegex

--- core/design-system/src/main/java/com/msg/design_system/util/checkRegex.kt
+++ core/design-system/src/main/java/com/msg/design_system/util/checkRegex.kt
@@ -4,3 +4,8 @@ fun String.checkEmailRegex(): Boolean {
     val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
     return this.matches(emailRegex.toRegex())
 }
+
+fun String.checkPasswordRegex(): Boolean {
+    val passwordRegex = "^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[@#$%^&+=!?.])(?=\\S+$).{8,24}.$"
+    return this.matches(passwordRegex.toRegex())
+}
