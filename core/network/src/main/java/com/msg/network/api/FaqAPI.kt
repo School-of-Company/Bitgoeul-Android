1ccccf240c9e388482003eb9fae13743130e129b
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-23 13:55:34 +0900

    :sparkles: :: addFAQ API interface suspend fun 작성
    :: addFAQ API interface suspend fun 작성

--- core/network/src/main/java/com/msg/network/api/FaqAPI.kt
+++ core/network/src/main/java/com/msg/network/api/FaqAPI.kt
@@ -1,10 +1,13 @@
 package com.msg.network.api
 
+import com.msg.model.remote.request.faq.FrequentlyAskedQuestionsRequest
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface FaqAPI {
     @POST("FAQ")
-    suspend fun addFrequentlyAskedQuestions() {
+    suspend fun addFrequentlyAskedQuestions(
+        @Body body: FrequentlyAskedQuestionsRequest,
+    )
 
-    }
 }
