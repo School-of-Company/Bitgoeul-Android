971e547a331ef67d690d326d38ac6062ccbc4b81
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-23 14:15:04 +0900

    :sparkles: :: addFaq RepositoryImpl 작성
    :: addFaq RepositoryImpl 작성

--- core/data/src/main/java/com/msg/data/repository/faq/FaqRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/faq/FaqRepository.kt
@@ -1,7 +1,8 @@
 package com.msg.data.repository.faq
 
 import com.msg.model.remote.request.faq.AddFrequentlyAskedQuestionsRequest
+import kotlinx.coroutines.flow.Flow
 
 interface FaqRepository {
-    suspend fun addFrequentlyAskedQuestions(body: AddFrequentlyAskedQuestionsRequest)
+    suspend fun addFrequentlyAskedQuestions(body: AddFrequentlyAskedQuestionsRequest): Flow<Unit>
 }
