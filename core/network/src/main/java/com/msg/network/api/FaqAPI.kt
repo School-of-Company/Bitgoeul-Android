2a784d23080b998148606e152614171aad325df6
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-27 01:09:42 +0900

    :sparkles: :: FAQ Detail 함수 API interface에 작성
    :: FAQ Detail 함수 API interface에 작성

--- core/network/src/main/java/com/msg/network/api/FaqAPI.kt
+++ core/network/src/main/java/com/msg/network/api/FaqAPI.kt
@@ -2,12 +2,20 @@ package com.msg.network.api
 
 import com.msg.model.remote.request.faq.AddFrequentlyAskedQuestionsRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.UUID
 
 interface FaqAPI {
     @POST("FAQ")
     suspend fun addFrequentlyAskedQuestions(
         @Body body: AddFrequentlyAskedQuestionsRequest,
+    )
+
+    @GET("FAQ/{id}")
+    suspend fun getFrequentlyAskedQuestionsDetail(
+        @Path("id") id: UUID,
     )
 
 }
