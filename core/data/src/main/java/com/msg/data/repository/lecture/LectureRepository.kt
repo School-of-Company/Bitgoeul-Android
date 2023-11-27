0b24571f314a02f68003a11e384dbe390f0f321f
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-27 16:13:31 +0900

    :recycle:  :: LectureRepository Interface getLectureList 함수 response 추가
     :: LectureRepository Interface getLectureList 함수 response 추가

--- core/data/src/main/java/com/msg/data/repository/lecture/LectureRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/lecture/LectureRepository.kt
@@ -2,12 +2,13 @@ package com.msg.data.repository.lecture
 
 import com.msg.model.remote.request.lecture.OpenLectureRequest
 import com.msg.model.remote.response.lecture.DetailLectureResponse
+import com.msg.model.remote.response.lecture.LectureListResponse
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface LectureRepository {
     suspend fun openLecture(body: OpenLectureRequest): Flow<Unit>
-    suspend fun getLectureList(): Flow<List<LectureListModel>>
+    suspend fun getLectureList(): Flow<List<LectureListResponse>>
     suspend fun getDetailLecture(id: UUID): Flow<DetailLectureResponse>
     suspend fun lectureApplication(id: UUID): Flow<Unit>
     suspend fun approvePendingLecture(id: UUID): Flow<Unit>
