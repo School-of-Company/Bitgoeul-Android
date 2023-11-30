c3fe86272c75b067a42312013255d0e8c8f6ae44
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-30 23:37:59 +0900

    :sparkles: :: CertificationDataSource 작성
    :: CertificationDataSource 작성

--- core/network/src/main/java/com/msg/network/datasource/certification/CertificationDataSource.kt
+++ core/network/src/main/java/com/msg/network/datasource/certification/CertificationDataSource.kt
@@ -1,5 +1,6 @@
 package com.msg.network.datasource.certification
 
+import com.msg.model.remote.request.certification.WriteCertificationRequest
 import com.msg.model.remote.response.certification.CertificationListResponse
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
@@ -7,5 +8,5 @@ import java.util.UUID
 interface CertificationDataSource {
     suspend fun getCertificationListForTeacher(studentId: UUID): Flow<List<CertificationListResponse>>
     suspend fun getCertificationListForStudent(): Flow<List<CertificationListResponse>>
-
+    suspend fun writeCertification(body: WriteCertificationRequest): Flow<Unit>
 }
