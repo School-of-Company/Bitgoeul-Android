0acf0849e70698609ae222dd764be04ce5677da5
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-02 20:11:32 +0900

    :fire: :: 불필요한 flowOn import 삭제
    :: 불필요한 flowOn import 삭제

--- core/network/src/main/java/com/msg/network/datasource/club/ClubDataSourceImpl.kt
+++ core/network/src/main/java/com/msg/network/datasource/club/ClubDataSourceImpl.kt
@@ -4,10 +4,8 @@ import com.msg.model.remote.enumdatatype.HighSchool
 import com.msg.model.remote.response.club.ClubListResponse
 import com.msg.network.api.ClubAPI
 import com.msg.network.util.BitgoeulApiHandler
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class ClubDataSourceImpl @Inject constructor(
