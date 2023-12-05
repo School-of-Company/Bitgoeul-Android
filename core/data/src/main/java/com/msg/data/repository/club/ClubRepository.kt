1ed6236d523afae468dd6d85f0c879505c029c28
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-05 17:58:47 +0900

    :sparkles: :: ClubRepository getStudentBelongClubList 함수 작성
    :: ClubRepository getStudentBelongClubList 함수 작성

--- core/data/src/main/java/com/msg/data/repository/club/ClubRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/club/ClubRepository.kt
@@ -3,9 +3,11 @@ package com.msg.data.repository.club
 import com.msg.model.remote.enumdatatype.HighSchool
 import com.msg.model.remote.response.club.ClubDetailResponse
 import com.msg.model.remote.response.club.ClubListResponse
+import com.msg.model.remote.response.club.StudentBelongClubResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ClubRepository {
     suspend fun getClubList(highSchool: HighSchool): Flow<List<ClubListResponse>>
     suspend fun getClubDetail(id: Long): Flow<ClubDetailResponse>
+    suspend fun getStudentBelongClubList(id: Long): Flow<List<StudentBelongClubResponse>>
 }
