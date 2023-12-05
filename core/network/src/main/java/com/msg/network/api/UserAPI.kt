319730ca8c27e6678b431db843b3e17bbf00d614
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-05 10:54:32 +0900

    :memo: add inquiryMaPage fun in UserAPI

--- core/network/src/main/java/com/msg/network/api/UserAPI.kt
+++ core/network/src/main/java/com/msg/network/api/UserAPI.kt
@@ -1,12 +1,17 @@
 package com.msg.network.api
 
 import com.msg.model.remote.request.user.ChangePasswordRequest
+import com.msg.model.remote.response.user.InquiryMyPageResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PATCH
 
 interface UserAPI {
-    @PATCH
+    @PATCH("user")
     suspend fun changePassword(
         @Body body: ChangePasswordRequest
     )
+
+    @GET("user")
+    suspend fun inquiryMyPage(): InquiryMyPageResponse
 }
