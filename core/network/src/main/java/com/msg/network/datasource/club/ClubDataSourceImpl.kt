e0852d6ddac1f953f6f83d502c207c57aaaab09f
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-05 15:20:27 +0900

    :sparkles: :: ClubDataSource getStudentBelongClubList 함수 작성
    :: ClubDataSource getStudentBelongClubList 함수 작성

--- core/network/src/main/java/com/msg/network/datasource/club/ClubDataSourceImpl.kt
+++ core/network/src/main/java/com/msg/network/datasource/club/ClubDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.msg.network.datasource.club
 import com.msg.model.remote.enumdatatype.HighSchool
 import com.msg.model.remote.response.club.ClubDetailResponse
 import com.msg.model.remote.response.club.ClubListResponse
+import com.msg.model.remote.response.club.StudentBelongClubResponse
 import com.msg.network.api.ClubAPI
 import com.msg.network.util.BitgoeulApiHandler
 import kotlinx.coroutines.flow.Flow
@@ -24,6 +25,14 @@ class ClubDataSourceImpl @Inject constructor(
         emit(
             BitgoeulApiHandler<ClubDetailResponse>()
                 .httpRequest { clubAPI.getClubDetail(id = id) }
+                .sendRequest()
+        )
+    }
+
+    override suspend fun getStudentBelongClubList(id: Long): Flow<List<StudentBelongClubResponse>> = flow {
+        emit(
+            BitgoeulApiHandler<List<StudentBelongClubResponse>>()
+                .httpRequest { clubAPI.getStudentBelongClubList(id = id) }
                 .sendRequest()
         )
     }
