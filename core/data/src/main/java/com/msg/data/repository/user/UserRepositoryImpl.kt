3d22363363987afdb88e07accdda35ec2d412a90
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-05 11:01:44 +0900

    :memo: add inquiryMyPage fun in UserRepository

--- core/data/src/main/java/com/msg/data/repository/user/UserRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/user/UserRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.msg.data.repository.user
 
 import com.msg.model.remote.request.user.ChangePasswordRequest
+import com.msg.model.remote.response.user.InquiryMyPageResponse
 import com.msg.network.datasource.user.UserDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -10,5 +11,9 @@ class UserRepositoryImpl @Inject constructor(
 ) : UserRepository {
     override suspend fun changePassword(body: ChangePasswordRequest): Flow<Unit> {
         return userDataSource.changePassword(body = body)
+    }
+
+    override suspend fun inquiryMyPage(): Flow<InquiryMyPageResponse> {
+        return userDataSource.inquiryMyPage()
     }
 }
