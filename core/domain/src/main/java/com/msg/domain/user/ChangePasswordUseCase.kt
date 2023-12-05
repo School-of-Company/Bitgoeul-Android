9e9f7ca5c2a1dde899fefb1f46f3fe1449e25df3
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-05 11:05:21 +0900

    :memo: add InquiryMyPageUseCase, ChangePasswordUseCase

---
+++ core/domain/src/main/java/com/msg/domain/user/ChangePasswordUseCase.kt
@@ -0,0 +1,13 @@
+package com.msg.domain.user
+
+import com.msg.data.repository.user.UserRepository
+import com.msg.model.remote.request.user.ChangePasswordRequest
+import javax.inject.Inject
+
+class ChangePasswordUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(body: ChangePasswordRequest) = runCatching {
+        userRepository.changePassword(body = body)
+    }
+}
