cd5c5b40a4e40b75eb7d4afcf6bfe1239837f8d1
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-07 01:17:10 +0900

    :recycle: :: GetLectureListUseCase invoke 함수 QueryString 인자값 추가
    :: GetLectureListUseCase invoke 함수 QueryString 인자값 추가

--- core/domain/src/main/java/com/msg/domain/lecture/GetLectureListUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/lecture/GetLectureListUseCase.kt
@@ -1,12 +1,14 @@
 package com.msg.domain.lecture
 
 import com.msg.data.repository.lecture.LectureRepository
+import com.msg.model.remote.enumdatatype.ApproveStatus
+import com.msg.model.remote.enumdatatype.LectureType
 import javax.inject.Inject
 
 class GetLectureListUseCase @Inject constructor(
     private val lectureRepository: LectureRepository
 ) {
-    suspend operator fun invoke() = runCatching {
-        lectureRepository.getLectureList()
+    suspend operator fun invoke(page: Int, size: Int, status: ApproveStatus, type: LectureType) = runCatching {
+        lectureRepository.getLectureList(page = page, size = size, status = status, type = type)
     }
 }
