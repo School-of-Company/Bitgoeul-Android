c018f3798eac1bac244e56b22ad0ebdfc72e2f4e
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-06 22:39:59 +0900

    :sparkles: :: Event 클래스 errorHandling 파일 작성
    :: Event 클래스 errorHandling 파일 작성

--- feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
+++ feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
@@ -1,12 +1,17 @@
 package com.msg.lecture.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.msg.lecture.util.Event
+import com.msg.datastore.AuthTokenDataSource
 import com.msg.domain.lecture.ApprovePendingLectureUseCase
 import com.msg.domain.lecture.GetDetailLectureUseCase
 import com.msg.domain.lecture.GetLectureListUseCase
 import com.msg.domain.lecture.LectureApplicationUseCase
 import com.msg.domain.lecture.OpenLectureUseCase
 import com.msg.domain.lecture.RejectPendingLectureUseCase
+import com.msg.model.remote.enumdatatype.Authority
+import com.msg.model.remote.response.lecture.LectureListResponse
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 class LectureViewModel @Inject constructor(
@@ -15,7 +20,9 @@ class LectureViewModel @Inject constructor(
     private val getLectureListUseCase: GetLectureListUseCase,
     private val lectureApplicationUseCase: LectureApplicationUseCase,
     private val openLectureUseCase: OpenLectureUseCase,
-    private val rejectPendingLectureUseCase: RejectPendingLectureUseCase
+    private val rejectPendingLectureUseCase: RejectPendingLectureUseCase,
+    private val authTokenDataSource: AuthTokenDataSource,
 ) : ViewModel() {
+    val role = Authority.valueOf(authTokenDataSource.getAuthority().toString())
 
 }
