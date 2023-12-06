c1bfd5e201e7d10c5f9ec5d4d76cfea55b7bc6a1
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-07 01:11:48 +0900

    :recycle: :: LectureAPI getLectureList 함수 QueryString 추가
    :: LectureAPI getLectureList 함수 QueryString 추가

--- feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
+++ feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
@@ -10,8 +10,10 @@ import com.msg.domain.lecture.LectureApplicationUseCase
 import com.msg.domain.lecture.OpenLectureUseCase
 import com.msg.domain.lecture.RejectPendingLectureUseCase
 import com.msg.model.remote.enumdatatype.Authority
+import com.msg.model.remote.response.lecture.DetailLectureResponse
 import com.msg.model.remote.response.lecture.LectureListResponse
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class LectureViewModel @Inject constructor(
@@ -24,5 +26,23 @@ class LectureViewModel @Inject constructor(
     private val authTokenDataSource: AuthTokenDataSource,
 ) : ViewModel() {
     val role = Authority.valueOf(authTokenDataSource.getAuthority().toString())
+
+    private val _getLectureListResponse = MutableStateFlow<Event<LectureListResponse>>(Event.Loading)
+    val getLectureListResponse = _getLectureListResponse.asStateFlow()
+
+    private val _getDetailLectureResponse = MutableStateFlow<Event<DetailLectureResponse>>(Event.Loading)
+    val getDetailLectureResponse = _getDetailLectureResponse.asStateFlow()
+
+    private val _openLectureResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val openLectureResponse = _openLectureResponse.asStateFlow()
+
+    private val _approvePendingLectureResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val approvePendingLectureResponse = _approvePendingLectureResponse.asStateFlow()
+
+    private val _rejectPendingLectureResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val rejectPendingLectureResponse = _rejectPendingLectureResponse
+
+    private val _lectureApplicationResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val lectureApplicationResponse = _lectureApplicationResponse.asStateFlow()
 
 }
