41eaa3095bdf496fc3145af69f6ff1be80b8f97d
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-06 16:13:35 +0900

    :sparkles: :: AdminRepository getUserList 함수 작성
    :: AdminRepository getUserList 함수 작성

---
+++ core/data/src/main/java/com/msg/data/repository/admin/AdminRepository.kt
@@ -0,0 +1,9 @@
+package com.msg.data.repository.admin
+
+import com.msg.model.remote.request.admin.GetUserListRequest
+import com.msg.model.remote.response.admin.UserListResponse
+import kotlinx.coroutines.flow.Flow
+
+interface AdminRepository {
+    suspend fun getUserList(body: GetUserListRequest, keyword: String): Flow<UserListResponse>
+}
