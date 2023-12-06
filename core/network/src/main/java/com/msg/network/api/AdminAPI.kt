6c3d4a8cdf73807bfe6633ca41e9ae8f3c480097
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-06 16:03:17 +0900

    :sparkles: :: AdminAPI getUserList 함수 작성
    :: AdminAPI getUserList 함수 작성

--- core/network/src/main/java/com/msg/network/api/AdminAPI.kt
+++ core/network/src/main/java/com/msg/network/api/AdminAPI.kt
@@ -1,5 +1,6 @@
 package com.msg.network.api
 
+import com.msg.model.remote.request.admin.GetUserListRequest
 import com.msg.model.remote.response.admin.UseListResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -8,7 +9,7 @@ import retrofit2.http.Query
 interface AdminAPI {
     @GET("admin")
     suspend fun getUserList(
-        @Body 
+        @Body body: GetUserListRequest,
         @Query("keyword") keyword: String,
     ): UseListResponse
 }
