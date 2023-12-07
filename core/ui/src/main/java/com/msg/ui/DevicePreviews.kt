f628fe44f726a1d688c05fbe8d90af3d5440ea2d
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-07 15:22:38 +0900

    :sparkles: add DevicePreviews

---
+++ core/ui/src/main/java/com/msg/ui/DevicePreviews.kt
@@ -0,0 +1,10 @@
+package com.msg.ui
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "phone_1", device = "spec:shape=Normal,width=360,height=880,unit=dp,dpi=480")
+@Preview(name = "phone_2", device = "spec:shape=Normal,width=412,height=732,unit=dp,dpi=420")
+@Preview(name = "s23", device = "spec:shape=Normal,width=360,height=780,unit=dp,dpi=480")
+@Preview(name = "foldable", device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480")
+@Preview(name = "tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480")
+annotation class DevicePreviews
