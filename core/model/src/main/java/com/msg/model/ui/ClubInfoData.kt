4c26db8d2e9f5213fb8561289f7ed43d16df7dc7
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-08 12:03:20 +0900

    :lipstick: main page publishing 40 percent save

---
+++ core/model/src/main/java/com/msg/model/ui/ClubInfoData.kt
@@ -0,0 +1,8 @@
+package com.msg.model.ui
+
+data class ClubInfoData(
+    val roleName: String,
+    val roleDetailName: String,
+    val primaryRoleContent: String,
+    val roleContent: List<String>
+)
