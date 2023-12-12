dec3dd3af636450198d3c7ee0a251c16e4098337
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-12-12 15:03:56 +0900

    :lipstick: add AutoSchoolClubGridView

--- feature/main/src/main/java/com/example/main/component/SchoolClubGridView.kt
+++ feature/main/src/main/java/com/example/main/component/SchoolClubGridView.kt
@@ -7,17 +7,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.msg.design_system.theme.BitgoeulAndroidTheme
 
 @Composable
-fun SchoolClubGridView(
+fun AutoSchoolClubGridView(
     modifier: Modifier = Modifier,
     school: String,
-    rows: Int,
-    rowItems: List<List<String>>,
+    rowItems: List<String>,
 ) {
+    val actualItems = rowItems.getActualList()
+
     BitgoeulAndroidTheme { colors, typography ->
         Column {
             Spacer(modifier = modifier.height(24.dp))
@@ -30,17 +38,63 @@ fun SchoolClubGridView(
             Column(
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                for (i in 0 until rows) {
+                for (element in actualItems) {
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(16.dp)
                     ) {
-                        rowItems[i].forEach {
+                        element.forEach {
                             ClubChipView(clubName = it)
                         }
                     }
                 }
             }
         }
-        
+
     }
+}
+
+@Composable
+fun List<String>.getActualList(): List<List<String>> {
+    val configuration = LocalConfiguration.current
+    val actualItems: MutableList<MutableList<String>> = mutableListOf(mutableListOf())
+
+    var width = 56.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    var rows = 0
+
+    for (element in this) {
+        if (width.plus(element.getClubChipWidth()) < screenWidth) {
+            width += element.getClubChipWidth() + 16.dp
+            actualItems[rows].add(element)
+        } else {
+            actualItems.add(mutableListOf(element))
+            width = 56.dp + element.getClubChipWidth() + 16.dp
+            rows += 1
+        }
+    }
+    return actualItems
+}
+
+@Composable
+fun String.getClubChipWidth(): Dp {
+    val width = remember { mutableStateOf(0.dp) }
+    val shouldShowCompose = remember { mutableStateOf(true) }
+    val density = LocalDensity.current
+    if (shouldShowCompose.value) {
+        ClubChipView(
+            clubName = this,
+            modifier = Modifier.onGloballyPositioned {
+                width.value = with(density) { it.size.width.toDp() }
+                return@onGloballyPositioned
+            }
+        )
+    }
+    shouldShowCompose.value = false
+    return width.value
+}
+
+@Preview
+@Composable
+fun LazyGridViewPre() {
+    AutoSchoolClubGridView(school = "gsm", rowItems = listOf("금융실무", "소개팅", "취사모"))
 }
