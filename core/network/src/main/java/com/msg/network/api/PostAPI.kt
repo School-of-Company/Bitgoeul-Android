b0da01ac8e20223f758437beee0825c7b8a5a760
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-03 20:57:26 +0900

    :memo: add getPostList fun in PostAPI

--- core/network/src/main/java/com/msg/network/api/PostAPI.kt
+++ core/network/src/main/java/com/msg/network/api/PostAPI.kt
@@ -1,10 +1,23 @@
 package com.msg.network.api
 
+import com.msg.model.remote.enumdatatype.FeedType
 import com.msg.model.remote.request.post.WritePostRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface PostAPI {
+
+    @POST("post")
     suspend fun sendPost(
         @Body body: WritePostRequest
+    )
+
+    @GET("post")
+    suspend fun getPostList(
+        @Query("type") type: FeedType,
+        @Query("size") size: Int,
+        @Query("page") page: Int
     )
 }
