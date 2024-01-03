82b8097e4437d0155edc14ea41b8c4c062a5b63e
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-03 20:59:19 +0900

    :memo: add getDetailPost fun in PostAPI

--- core/network/src/main/java/com/msg/network/api/PostAPI.kt
+++ core/network/src/main/java/com/msg/network/api/PostAPI.kt
@@ -2,10 +2,14 @@ package com.msg.network.api
 
 import com.msg.model.remote.enumdatatype.FeedType
 import com.msg.model.remote.request.post.WritePostRequest
+import com.msg.model.remote.response.post.GetDetailPostResponse
+import com.msg.model.remote.response.post.GetPostListResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.UUID
 
 interface PostAPI {
 
@@ -19,5 +23,10 @@ interface PostAPI {
         @Query("type") type: FeedType,
         @Query("size") size: Int,
         @Query("page") page: Int
-    )
+    ): GetPostListResponse
+
+    @GET("post/{id}")
+    suspend fun getDetailPost(
+        @Path("id") id: UUID
+    ): GetDetailPostResponse
 }
