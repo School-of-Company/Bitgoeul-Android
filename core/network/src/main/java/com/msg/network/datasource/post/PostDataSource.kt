a3c2837a51ed0be9b55f237c1f8449406d66b915
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-04 11:36:22 +0900

    :memo: add sendPost fun in PostDataSource

--- core/network/src/main/java/com/msg/network/datasource/post/PostDataSource.kt
+++ core/network/src/main/java/com/msg/network/datasource/post/PostDataSource.kt
@@ -1,4 +1,8 @@
 package com.msg.network.datasource.post
 
+import com.msg.model.remote.request.post.WritePostRequest
+import kotlinx.coroutines.flow.Flow
+
 interface PostDataSource {
+    suspend fun sendPost(body: WritePostRequest): Flow<Unit>
 }
