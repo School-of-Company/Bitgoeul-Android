07ae5493c48aacd1c2f8971f66e6a28d801b0f3e
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-04 11:36:22 +0900

    :memo: add sendPost fun in PostDataSource

--- core/network/src/main/java/com/msg/network/datasource/post/PostDataSourceImpl.kt
+++ core/network/src/main/java/com/msg/network/datasource/post/PostDataSourceImpl.kt
@@ -1,9 +1,22 @@
 package com.msg.network.datasource.post
 
+import com.msg.model.remote.request.post.WritePostRequest
 import com.msg.network.api.PostAPI
+import com.msg.network.util.BitgoeulApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class PostDataSourceImpl @Inject constructor(
     private val postAPI: PostAPI
 ) : PostDataSource {
+    override suspend fun sendPost(body: WritePostRequest): Flow<Unit> = flow {
+        emit(
+            BitgoeulApiHandler<Unit>()
+                .httpRequest { postAPI.sendPost(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
 }
