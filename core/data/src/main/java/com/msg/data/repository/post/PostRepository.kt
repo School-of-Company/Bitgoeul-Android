3c1516fdecbaeff91d8c3f552dee9710bfd743e5
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-08 10:40:03 +0900

    :memo: add sendPost fun in PostRepository

--- core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
@@ -1,4 +1,8 @@
 package com.msg.data.repository.post
 
+import com.msg.model.remote.request.post.WritePostRequest
+import kotlinx.coroutines.flow.Flow
+
 interface PostRepository {
+    suspend fun sendPost(body: WritePostRequest): Flow<Unit>
 }
