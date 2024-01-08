762d6a9ac67dee2c032418bae594fe542fabea49
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-08 10:45:09 +0900

    :memo: add getPostList fun in PostRepository

--- core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
@@ -1,8 +1,11 @@
 package com.msg.data.repository.post
 
+import com.msg.model.remote.enumdatatype.FeedType
 import com.msg.model.remote.request.post.WritePostRequest
+import com.msg.model.remote.response.post.GetPostListResponse
 import kotlinx.coroutines.flow.Flow
 
 interface PostRepository {
     suspend fun sendPost(body: WritePostRequest): Flow<Unit>
+    suspend fun getPostList(type: FeedType, size: Int, page: Int): Flow<GetPostListResponse>
 }
