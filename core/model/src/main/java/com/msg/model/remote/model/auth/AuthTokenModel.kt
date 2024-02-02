eb84ba285f3086cc958aee437215725e8544bbf0
Chaejongin12 <s22055@gsm.hs.kr>
2024-02-02 20:17:21 +0900

    :recycle: :: AuthTokenModel ExpiredAt 타입 변경
    :: AuthTokenModel ExpiredAt 타입 변경

--- core/model/src/main/java/com/msg/model/remote/model/auth/AuthTokenModel.kt
+++ core/model/src/main/java/com/msg/model/remote/model/auth/AuthTokenModel.kt
@@ -1,12 +1,13 @@
 package com.msg.model.remote.model.auth
 
 import com.msg.model.remote.enumdatatype.Authority
+import java.time.LocalDateTime
 
 
 data class AuthTokenModel(
     val accessToken: String,
     val refreshToken: String,
-    val accessExpiredAt: String,
-    val refreshExpiredAt: String,
+    val accessExpiredAt: LocalDateTime,
+    val refreshExpiredAt: LocalDateTime,
     val authority: Authority
 )
