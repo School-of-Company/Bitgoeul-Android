a4f44985de234320efc211a0fdb6e5adf44ca90b
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-03-12 13:38:31 +0900

    :memo: add variable

--- feature/post/src/main/java/com/msg/post/PostViewModel.kt
+++ feature/post/src/main/java/com/msg/post/PostViewModel.kt
@@ -1,15 +1,26 @@
 package com.msg.post
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.msg.domain.post.DeletePostUseCase
 import com.msg.domain.post.EditPostUseCase
 import com.msg.domain.post.GetDetailPostUseCase
 import com.msg.domain.post.GetPostListUseCase
 import com.msg.domain.post.SendPostUseCase
+import com.msg.model.remote.enumdatatype.FeedType
+import com.msg.model.remote.request.post.WritePostRequest
+import com.msg.model.remote.response.post.GetDetailPostResponse
+import com.msg.model.remote.response.post.GetPostListResponse
 import com.msg.post.util.Event
+import com.msg.post.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,4 +32,37 @@ class PostViewModel @Inject constructor(
     private val sendPostUseCase: SendPostUseCase
 ) : ViewModel() {
 
+    private val _deletePostResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val deletePostResponse = _deletePostResponse.asStateFlow()
+
+    private val _editPostResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val editPostResponse = _editPostResponse.asStateFlow()
+
+    private val _getDetailPostResponse = MutableStateFlow<Event<GetDetailPostResponse>>(Event.Loading)
+    val getDetailPostResponse = _getDetailPostResponse.asStateFlow()
+
+    private val _getPostListResponse = MutableStateFlow<Event<GetPostListResponse>>(Event.Loading)
+    val getPostListResponse = _getPostListResponse.asStateFlow()
+
+    private val _sendPostResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val sendPostResponse = _sendPostResponse.asStateFlow()
+
+    var detailPost = mutableStateOf(
+        GetDetailPostResponse(
+            title = "",
+            writer = "",
+            content = "",
+            feedType = FeedType.NOTICE,
+            modifiedAt = LocalDateTime.now(),
+            links = listOf()
+        )
+    )
+        private set
+
+    var postList = mutableStateOf(
+        GetPostListResponse(
+            posts = listOf()
+        )
+    )
+        private set
 }
