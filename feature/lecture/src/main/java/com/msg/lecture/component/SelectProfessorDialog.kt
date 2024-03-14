d61741810ab7db5b30d20184c7058942957bb6bc
Chaejongin12 <s22055@gsm.hs.kr>
2024-03-14 18:13:56 +0900

    :lipstick: :: SelectProfessorDialog isVisible 추가
    :: SelectProfessorDialog isVisible 추가

--- feature/lecture/src/main/java/com/msg/lecture/component/SelectProfessorDialog.kt
+++ feature/lecture/src/main/java/com/msg/lecture/component/SelectProfessorDialog.kt
@@ -29,59 +29,62 @@ import com.msg.design_system.theme.BitgoeulAndroidTheme
 @Composable
 fun SelectProfessorDialog(
     modifier: Modifier = Modifier,
-) {
-    BitgoeulAndroidTheme { colors, typography ->
-        Dialog(onDismissRequest = {}) {
-            Surface {
-                Box(
-                    modifier = modifier
-                        .background(color = colors.WHITE, RoundedCornerShape(8.dp))
-                        .wrapContentHeight()
-                ) {
-                    Spacer(
+    isVisible: Boolean,
+    ) {
+    if (isVisible) {
+        BitgoeulAndroidTheme { colors, typography ->
+            Dialog(onDismissRequest = {}) {
+                Surface {
+                    Box(
                         modifier = modifier
-                            .padding(top = 150.dp)
-                            .height(1.dp)
-                            .fillMaxWidth()
-                            .background(color = colors.G9)
-                    )
-                    Column(
-                        modifier = modifier.padding(horizontal = 24.dp)
+                            .background(color = colors.WHITE, RoundedCornerShape(8.dp))
+                            .wrapContentHeight()
                     ) {
-                        Row(
+                        Spacer(
                             modifier = modifier
+                                .padding(top = 150.dp)
+                                .height(1.dp)
                                 .fillMaxWidth()
-                                .padding(top = 24.dp, bottom = 24.dp)
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.select_professor_in_charge),
-                                color = colors.BLACK,
-                                style = typography.labelSmall,
-                                modifier = modifier
-                                    .weight(0.7f)
-                            )
-                            BlackCloseIcon(
-                                modifier = modifier
-                                    .align(Alignment.CenterVertically)
-                            )
-                        }
-                        TrailingIconTextField(
-                            modifier = modifier
-                                .padding(bottom = 16.dp)
-                                .fillMaxWidth()
-                                .height(54.dp),
-                            placeholder = "이름 또는 학교명으로 검색...",
-                            onValueChange = {},
-                            isDisabled = false,
-                            onClick = {},
-                            onClickButton = {}
+                                .background(color = colors.G9)
                         )
+                        Column(
+                            modifier = modifier.padding(horizontal = 24.dp)
+                        ) {
+                            Row(
+                                modifier = modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 24.dp, bottom = 24.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.select_professor_in_charge),
+                                    color = colors.BLACK,
+                                    style = typography.labelSmall,
+                                    modifier = modifier
+                                        .weight(0.7f)
+                                )
+                                BlackCloseIcon(
+                                    modifier = modifier
+                                        .align(Alignment.CenterVertically)
+                                )
+                            }
+                            TrailingIconTextField(
+                                modifier = modifier
+                                    .padding(bottom = 16.dp)
+                                    .fillMaxWidth()
+                                    .height(54.dp),
+                                placeholder = "이름 또는 학교명으로 검색...",
+                                onValueChange = {},
+                                isDisabled = false,
+                                onClick = {},
+                                onClickButton = {}
+                            )
 
-                        ProfessorInChargeList(modifier = modifier)
+                            ProfessorInChargeList(modifier = modifier)
 
+                        }
                     }
-                }
 
+                }
             }
         }
     }
@@ -95,6 +98,6 @@ fun SelectProfessorDialogPre() {
             .fillMaxSize()
             .background(Color.Blue)
     ) {
-        SelectProfessorDialog(modifier = Modifier)
+        SelectProfessorDialog(modifier = Modifier, isVisible = true)
     }
 }
