558a8a6fee4f9579eef29e6332e8dcce919d7d92
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-03-19 09:51:17 +0900

    :memo: add PostNavigation

---
+++ feature/post/src/main/java/com/msg/post/navigation/PostNavigation.kt
@@ -0,0 +1,29 @@
+package com.msg.post.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.msg.post.PostScreenRoute
+import java.util.UUID
+
+const val postRoute = "post_route"
+const val postDetailRoute = "post_detail_route"
+const val postAddRoute = "post_Add_route"
+const val postDetailSettingRoute = "post_detail_setting_route"
+
+fun NavController.navigateToPostPage(navOptions: NavOptions? = null) {
+    this.navigate(postRoute, navOptions)
+}
+
+fun NavController.navigateToPostDetailPage(navOptions: NavOptions? = null) {
+    this.navigate(postDetailRoute, navOptions)
+}
+
+fun NavController.navigateToPostAddPage(navOptions: NavOptions? = null) {
+    this.navigate(postAddRoute, navOptions)
+}
+
+fun NavController.navigateToPostDetailSettingPage(navOptions: NavOptions? = null) {
+    this.navigate(postDetailSettingRoute, navOptions)
+}
