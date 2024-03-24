bbd2f4de29f4e06f9b4a8dc82c19366e571f8257
Chaejongin12 <s22055@gsm.hs.kr>
2024-03-25 08:53:33 +0900

    :sparkles: :: LectureRepository 작성
    :: LectureRepository 작성

--- core/data/src/main/java/com/msg/data/repository/lecture/LectureRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/lecture/LectureRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.msg.data.repository.lecture
 
+import com.msg.model.remote.enumdatatype.Division
 import com.msg.model.remote.enumdatatype.LectureType
+import com.msg.model.remote.model.lecture.SearchResponseModel
 import com.msg.model.remote.request.lecture.OpenLectureRequest
 import com.msg.model.remote.response.lecture.DetailLectureResponse
 import com.msg.model.remote.response.lecture.LectureListResponse
+import com.msg.model.remote.response.lecture.SearchProfessorResponse
 import com.msg.network.datasource.lecture.LectureDataSource
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
@@ -39,6 +42,31 @@ class LectureRepositoryImpl @Inject constructor(
     override suspend fun lectureApplication(id: UUID): Flow<Unit> {
         return lectureDataSource.lectureApplication(
             id = id
+        )
+    }
+
+    override suspend fun lectureApplicationCancel(id: UUID): Flow<Unit> {
+        return lectureDataSource.lectureApplicationCancel(
+            id = id
+        )
+    }
+
+    override suspend fun searchProfessor(keyword: String): Flow<List<SearchProfessorResponse>> {
+        return lectureDataSource.searchProfessor(
+            keyword = keyword
+        )
+    }
+
+    override suspend fun searchLine(keyword: String, division: Division): Flow<SearchResponseModel> {
+        return lectureDataSource.searchLine(
+            keyword = keyword,
+            division = division
+        )
+    }
+
+    override suspend fun searchDepartment(keyword: String): Flow<SearchResponseModel> {
+        return lectureDataSource.searchDepartment(
+            keyword = keyword
         )
     }
 
