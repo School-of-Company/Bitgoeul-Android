7a3450e38cc7e0d99abc979bbb0e53d9e89673f9
Chaejongin12 <s22055@gsm.hs.kr>
2024-03-25 13:51:34 +0900

    :recycle: :: LectureDetailData 변수값 변경에 따라 수정
    :: LectureDetailData 변수값 변경에 따라 수정

--- feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
+++ feature/lecture/src/main/java/com/msg/lecture/viewmodel/LectureViewModel.kt
@@ -22,8 +22,8 @@ import com.msg.model.remote.enumdatatype.Division
 import com.msg.model.remote.enumdatatype.LectureStatus
 import com.msg.model.remote.enumdatatype.LectureType
 import com.msg.model.remote.enumdatatype.Semester
+import com.msg.model.remote.model.lecture.LectureDates
 import com.msg.model.remote.model.lecture.SearchResponseModel
-import com.msg.model.remote.response.lecture.*
 import com.msg.model.remote.request.lecture.OpenLectureRequest
 import com.msg.model.remote.response.lecture.ContentArray
 import com.msg.model.remote.response.lecture.DetailLectureResponse
@@ -141,23 +141,7 @@ class LectureViewModel @Inject constructor(
         )
     )
         private set
-//    val name: String,
-//    val content: String,
-//    val semester: Semester,
-//    val division: Division,
-//    val department: String,
-//    val line: String,
-//    val createAt: LocalDate,
-//    val startDate: LocalDateTime,
-//    val endDate: LocalDateTime,
-//    val lectureDates: List<com.msg.model.remote.response.lecture.LectureDates>,
-//    val lectureType: LectureType,
-//    val lectureStatus: LectureStatus,
-//    val headCount: Int,
-//    val maxRegisteredUser: Int,
-//    val isRegistered: Boolean,
-//    val lecturer: String,
-//    val credit: Int,
+
     var selectedLectureId = mutableStateOf<UUID>(UUID.randomUUID())
         private set
 
