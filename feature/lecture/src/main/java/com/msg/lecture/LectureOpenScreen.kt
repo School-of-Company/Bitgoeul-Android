945666ca3b084a337a58ba535cd8c8a3543ae6c9
Chaejongin12 <s22055@gsm.hs.kr>
2024-03-26 16:16:04 +0900

    :sparkles: :: LectureOpen Screen단에서 API 연결
    :: LectureOpen Screen단에서 API 연결

--- feature/lecture/src/main/java/com/msg/lecture/LectureOpenScreen.kt
+++ feature/lecture/src/main/java/com/msg/lecture/LectureOpenScreen.kt
@@ -13,32 +13,94 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.msg.design_system.component.button.BitgoeulButton
+import com.msg.design_system.component.button.ButtonState
 import com.msg.design_system.component.button.DetailSettingButton
+import com.msg.design_system.component.dialog.PositiveActionDialog
 import com.msg.design_system.component.icon.GoBackIcon
 import com.msg.design_system.component.textfield.InputMainContentTextField
 import com.msg.design_system.component.textfield.InputTitleTextField
 import com.msg.design_system.component.topbar.GoBackTopBar
 import com.msg.design_system.theme.BitgoeulAndroidTheme
 import com.msg.lecture.viewmodel.LectureViewModel
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayAt
 
 @Composable
 fun LectureOpenRoute(
+    onActionClicked: () -> Unit,
+    onBackClicked: () -> Unit,
     onSettingClicked: () -> Unit,
     viewModel: LectureViewModel = hiltViewModel()
 ) {
+    val currentDate = Clock.System.todayAt(TimeZone.currentSystemDefault())
+    val currentDateTime: LocalDateTime =
+        Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+    val currentTime: LocalTime =
+        Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).let {
+            LocalTime(it.hour, it.minute, it.second)
+        }
 
+
+    LectureOpenScreen(
+        onActionClicked = {
+            onActionClicked()
+            viewModel.openLecture(
+                name = viewModel.name.value,
+                content = viewModel.content.value,
+                semester = viewModel.semester.value,
+                division = viewModel.division.value,
+                department = viewModel.department.value,
+                line = viewModel.line.value,
+                userId = viewModel.userId.value,
+                startDate = viewModel.startDate.value ?: currentDateTime,
+                endDate = viewModel.endDate.value ?: currentDateTime,
+                completeDate = viewModel.completeDate.value ?: currentDate,
+                startTime = viewModel.startTime.value ?: currentTime,
+                endTime = viewModel.endTime.value ?: currentTime,
+                lectureType = viewModel.lectureType.value,
+                credit = viewModel.credit.value,
+                maxRegisteredUser = viewModel.maxRegisteredUser.value,
+            )
+        },
+        onBackClicked = onBackClicked,
+        onSettingClicked = { name, content ->
+            viewModel.name.value = name
+            viewModel.content.value = content
+            onSettingClicked()
+        },
+        savedName = viewModel.name.value,
+        savedContent = viewModel.content.value
+    )
 }
 
 @Composable
 fun LectureOpenScreen(
-    onBackClick: () -> Unit,
+    onActionClicked: () -> Unit,
+    onBackClicked: () -> Unit,
+    onSettingClicked: (name: String, content: String) -> Unit,
+    savedName: String,
+    savedContent: String,
 ) {
+    val name = remember { mutableStateOf(savedName) }
+    val content = remember { mutableStateOf(savedContent) }
+
+    val isDialogVisible = remember { mutableStateOf(false) }
+
+    val maxTitleLength = 100
+    val maxContentLength = 1000
+
     val scrollState = rememberScrollState()
 
     BitgoeulAndroidTheme { colors, type ->
@@ -58,12 +120,49 @@ fun LectureOpenScreen(
                 GoBackTopBar(
                     icon = { GoBackIcon() },
                     text = "돌아가기",
-                    onClick = { onBackClick() }
+                    onClick = { onBackClicked() }
                 )
 
                 Spacer(modifier = Modifier.height(17.dp))
 
-                LectureOpenScreenContent()
+                Column(
+                    modifier = Modifier
+                        .background(color = colors.WHITE)
+                        .wrapContentSize()
+                        .padding(horizontal = 24.dp),
+
+                    ) {
+                    InputTitleTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        placeholder = "강의 제목 (100자 이내)",
+                        onClick = {},
+                        onClickButton = {},
+                        onValueChange = {},
+                        maxTextLength = maxTitleLength
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(1.dp)
+                            .fillMaxWidth()
+                            .background(color = colors.G9)
+                    )
+
+                    InputMainContentTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        placeholder = "본문 입력 (1000자 이내)",
+                        onClick = {},
+                        onClickButton = {},
+                        onValueChange = {},
+                        maxTextLength = maxContentLength
+                    )
+                }
             }
 
             Spacer(
@@ -85,7 +184,9 @@ fun LectureOpenScreen(
                     .height(52.dp)
                     .padding(horizontal = 24.dp),
                 type = "강의"
-            ) {}
+            ) {
+                onSettingClicked(name.value, content.value)
+            }
 
             BitgoeulButton(
                 text = "강의 개설 신청",
@@ -95,58 +196,32 @@ fun LectureOpenScreen(
                     .fillMaxWidth()
                     .height(52.dp)
                     .padding(horizontal = 24.dp),
+                state = if (name.value.isNotEmpty() && content.value.isNotEmpty()) ButtonState.Enable else ButtonState.Disable
             ) {
+                isDialogVisible.value = true
             }
         }
+
+        PositiveActionDialog(
+            title = "강의 개설하시겠습니까?",
+            positiveAction = "개설",
+            content = name.value,
+            isVisible = isDialogVisible.value,
+            onQuit = { isDialogVisible.value = false },
+            onActionClicked = onActionClicked
+        )
     }
 }
 
-@Composable
-fun LectureOpenScreenContent() {
-    BitgoeulAndroidTheme { colors, type ->
-        Column(
-            modifier = Modifier
-                .background(color = colors.WHITE)
-                .wrapContentSize()
-                .padding(horizontal = 24.dp),
-
-            ) {
-            InputTitleTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                placeholder = "강의 제목 (100자 이내)",
-                onClick = {},
-                onClickButton = {},
-                onValueChange = {},
-                maxTextLength = 100
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Spacer(
-                modifier = Modifier
-                    .height(1.dp)
-                    .fillMaxWidth()
-                    .background(color = colors.G9)
-            )
-
-            InputMainContentTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                placeholder = "본문 입력 (1000자 이내)",
-                onClick = {},
-                onClickButton = {},
-                onValueChange = {},
-                maxTextLength = 1000
-            )
-        }
-    }
-}
 
 @Preview
 @Composable
 fun LectureOpenScreenPre() {
-    LectureOpenScreen(onBackClick = {})
+    LectureOpenScreen(
+        onActionClicked = {},
+        onSettingClicked = { _, _ -> },
+        savedName = "",
+        savedContent = "",
+        onBackClicked = {}
+    )
 }
