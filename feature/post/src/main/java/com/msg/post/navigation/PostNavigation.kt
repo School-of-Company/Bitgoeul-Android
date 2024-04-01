6099d077ead9bd8a997a0e66ac9451d9f15e2d49
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-04-01 09:18:32 +0900

    :memo: add screen function in Navigation

--- feature/post/src/main/java/com/msg/post/navigation/PostNavigation.kt
+++ feature/post/src/main/java/com/msg/post/navigation/PostNavigation.kt
@@ -4,6 +4,9 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.msg.post.PostAddScreenRoute
+import com.msg.post.PostDetailScreenRoute
+import com.msg.post.PostDetailSettingScreenRoute
 import com.msg.post.PostScreenRoute
 import java.util.UUID
 
@@ -29,10 +32,38 @@ fun NavController.navigateToPostDetailPage(navOptions: NavOptions? = null) {
     this.navigate(postDetailRoute, navOptions)
 }
 
+fun NavGraphBuilder.postDetailScreen(onEditClicked: () -> Unit, onDeleteClicked: () -> Unit, onBackClicked: () -> Unit) {
+    composable(route = postDetailRoute) {
+        PostDetailScreenRoute(
+            onEditClicked = onEditClicked,
+            onDeleteClicked = onDeleteClicked,
+            onBackClicked = onBackClicked
+        )
+    }
+}
+
 fun NavController.navigateToPostAddPage(navOptions: NavOptions? = null) {
     this.navigate(postAddRoute, navOptions)
 }
 
+fun NavGraphBuilder.postAddScreen(onBackClicked: () -> Unit, onSettingClicked: () -> Unit, onAddClicked: () -> Unit) {
+    composable(route = postAddRoute) {
+        PostAddScreenRoute(
+            onBackClicked = onBackClicked,
+            onSettingClicked = onSettingClicked,
+            onAddClicked = onAddClicked
+        )
+    }
+}
+
 fun NavController.navigateToPostDetailSettingPage(navOptions: NavOptions? = null) {
     this.navigate(postDetailSettingRoute, navOptions)
+}
+
+fun NavGraphBuilder.postDetailSettingScreen(onCloseClicked: () -> Unit) {
+    composable(route = postDetailSettingRoute) {
+        PostDetailSettingScreenRoute(
+            onCloseClick = onCloseClicked
+        )
+    }
 }
