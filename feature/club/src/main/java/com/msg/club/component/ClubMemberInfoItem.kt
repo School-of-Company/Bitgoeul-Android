fefc8bfb344027749f86515ceb3b4fed0b9152aa
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-04-07 12:49:12 +0900

    :lipstick: add MemberInfoItem

---
+++ feature/club/src/main/java/com/msg/club/component/ClubMemberInfoItem.kt
@@ -0,0 +1,45 @@
+package com.msg.club.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.msg.design_system.theme.BitgoeulAndroidTheme
+import com.msg.model.remote.response.club.ClubDetailResponse
+import java.util.UUID
+
+@Composable
+fun ClubMemberInfoItem(
+    modifier: Modifier = Modifier,
+    data: String,
+    isTeacher: Boolean,
+    id: UUID,
+    onItemClicked: (UUID) -> Unit
+) {
+    BitgoeulAndroidTheme { colors, typography ->
+        Row(
+            modifier = modifier
+                .padding(vertical = 8.dp)
+                .clickable {
+                    onItemClicked(id)
+                }
+        ) {
+            Text(
+                text = data,
+                style = typography.labelMedium,
+                color = colors.BLACK
+            )
+            Spacer(modifier = modifier.weight(1f))
+            Text(
+                text = if (isTeacher) "담당 선생님" else "학생",
+                style = typography.labelMedium,
+                color = colors.G2
+            )
+        }
+    }
+}
