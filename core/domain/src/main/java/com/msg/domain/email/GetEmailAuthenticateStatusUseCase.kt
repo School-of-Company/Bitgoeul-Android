2bc0a4d6d67fdb0bfab232a1d50781f0733a20ce
Chaejongin12 <s22055@gsm.hs.kr>
2024-04-26 10:01:47 +0900

    :sparkles: :: GetEmailAuthenticateStatusUseCase wkrtjd
    :: GetEmailAuthenticateStatusUseCase wkrtjd

---
+++ core/domain/src/main/java/com/msg/domain/email/GetEmailAuthenticateStatusUseCase.kt
@@ -0,0 +1,12 @@
+package com.msg.domain.email
+
+import com.msg.data.repository.email.EmailRepository
+import javax.inject.Inject
+
+class GetEmailAuthenticateStatusUseCase @Inject constructor(
+    private val emailRepository: EmailRepository
+) {
+    suspend operator fun invoke(email: String) = runCatching {
+        emailRepository.getEmailAuthenticateStatus(email = email)
+    }
+}
