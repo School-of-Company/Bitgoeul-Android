846b82eb735831cc4753f51082d382889651ea09
Chaejongin12 <s22055@gsm.hs.kr>
2024-04-29 09:33:38 +0900

    :sparkles: :: EmailSendInformRoute Navigation 파일에 등록
    :: EmailSendInformRoute Navigation 파일에 등록

--- feature/email/src/main/java/com/bitgoeul/email/navigation/EmailNavigation.kt
+++ feature/email/src/main/java/com/bitgoeul/email/navigation/EmailNavigation.kt
@@ -4,12 +4,19 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.bitgoeul.email.EmailSendInformRoute
 import com.bitgoeul.email.InputEmailRoute
 
 const val inputEmailRoute = "input_email_route"
 
+const val emailSendInformRoute = "email_send_inform_route"
+
 fun NavController.navigateToInputEmail(navOptions: NavOptions? = null) {
     this.navigate(inputEmailRoute, navOptions)
+}
+
+fun NavController.navigateToEmailSendInform(navOptions: NavOptions? = null) {
+    this.navigate(emailSendInformRoute, navOptions)
 }
 
 fun NavGraphBuilder.inputEmailScreen(
@@ -20,6 +27,18 @@ fun NavGraphBuilder.inputEmailScreen(
         InputEmailRoute(
             onBackClicked = onBackClicked,
             onNextClicked = onNextClicked,
+        )
+    }
+}
+
+fun NavGraphBuilder.emailSendInformScreen(
+    onBackClicked: () -> Unit,
+    onMoveNewPasswordClick: () -> Unit,
+) {
+    composable(route = emailSendInformRoute) {
+        EmailSendInformRoute(
+            onBackClicked = onBackClicked,
+            onMoveNewPasswordClick = onMoveNewPasswordClick,
         )
     }
 }
