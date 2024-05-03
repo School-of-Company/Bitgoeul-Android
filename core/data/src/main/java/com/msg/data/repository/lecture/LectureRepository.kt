06e7ec450817b56bdb8c28fe91bedb87e59b38e7
Chaejongin12 <s22055@gsm.hs.kr>
2024-05-03 09:28:16 +0900

    :sparkles: :: LectureRepository 신규 기능 추가
    :: LectureRepository 신규 기능 추가

--- core/data/src/main/java/com/msg/data/repository/lecture/LectureRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/lecture/LectureRepository.kt
@@ -3,8 +3,11 @@ package com.msg.data.repository.lecture
 import com.msg.model.remote.enumdatatype.LectureType
 import com.msg.model.remote.request.lecture.OpenLectureRequest
 import com.msg.model.remote.response.lecture.DetailLectureResponse
+import com.msg.model.remote.response.lecture.GetLectureSignUpHistoryResponse
+import com.msg.model.remote.response.lecture.GetTakingLectureStudentListResponse
 import com.msg.model.remote.response.lecture.LectureListResponse
 import com.msg.model.remote.response.lecture.SearchDepartmentResponse
+import com.msg.model.remote.response.lecture.SearchDivisionResponse
 import com.msg.model.remote.response.lecture.SearchLineResponse
 import com.msg.model.remote.response.lecture.SearchProfessorResponse
 import kotlinx.coroutines.flow.Flow
@@ -19,4 +22,9 @@ interface LectureRepository {
     suspend fun searchProfessor(keyword: String): Flow<SearchProfessorResponse>
     suspend fun searchLine(keyword: String, division: String): Flow<SearchLineResponse>
     suspend fun searchDepartment(keyword: String): Flow<SearchDepartmentResponse>
+    suspend fun searchDivision(keyword: String): Flow<SearchDivisionResponse>
+    suspend fun getLectureSignUpHistory(studentId: UUID): Flow<GetLectureSignUpHistoryResponse>
+    suspend fun getTakingLectureStudentList(id: UUID): Flow<GetTakingLectureStudentListResponse>
+    suspend fun editPost(id: UUID, studentId: UUID, isComplete: Boolean): Flow<Unit>
+
 }
