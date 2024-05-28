37fcde42640bae077ca47e66f6c3c152ddb4cb1c
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-05-28 11:06:26 +0900

    :sparkles: add BottomNavigation

--- app/src/main/java/com/msg/bitgoeul_android/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/msg/bitgoeul_android/navigation/TopLevelDestination.kt
@@ -1,10 +1,30 @@
 package com.msg.bitgoeul_android.navigation
 
-enum class TopLevelDestination {
-    LOGIN,
-    SIGN_UP,
-    STUDENT_ACTIVITY,
-    MY_PAGE,
-    POST,
-    CLUB
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.msg.design_system.R
+
+enum class TopLevelDestination(
+    val unselectedIcon: Int,
+    val iconText: String
+) {
+    LECTURE(
+        unselectedIcon = R.drawable.ic_lecture,
+        iconText = "강의"
+    ),
+    CLUB(
+        unselectedIcon = R.drawable.ic_club,
+        iconText = "동아리"
+    ),
+    MAIN(
+        unselectedIcon = R.drawable.ic_home,
+        iconText = "홈"
+    ),
+    POST(
+        unselectedIcon = R.drawable.ic_board,
+        iconText = "게시판"
+    ),
+    MY_PAGE(
+        unselectedIcon = R.drawable.ic_my,
+        iconText = "내 정보"
+    )
 }
--- app/src/main/java/com/msg/bitgoeul_android/ui/BitgoeulApp.kt
+++ app/src/main/java/com/msg/bitgoeul_android/ui/BitgoeulApp.kt
@@ -1,10 +1,36 @@
 package com.msg.bitgoeul_android.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.semantics
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.my_page.navigation.myPageRoute
 import com.msg.bitgoeul_android.navigation.BitgoeulNavHost
+import com.msg.bitgoeul_android.navigation.TopLevelDestination
+import com.msg.club.navigation.clubRoute
+import com.msg.design_system.component.navigation.BitgoeulNavigationBar
+import com.msg.design_system.component.navigation.BitgoeulNavigationBarItem
 import com.msg.design_system.theme.BitgoeulAndroidTheme
+import com.msg.design_system.theme.Typography
+import com.msg.design_system.theme.color.BitgoeulColor
+import com.msg.lecture.navigation.lectureListRoute
+import com.msg.main.navigation.mainPageRoute
+import com.msg.post.navigation.postRoute
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BitgoeulApp(
     windowSizeClass: WindowSizeClass,
@@ -12,7 +38,78 @@ fun BitgoeulApp(
         windowSizeClass = windowSizeClass
     )
 ) {
-    BitgoeulAndroidTheme { _, _ ->
-        BitgoeulNavHost(appState = appState)
+    val isBottomBarVisible = remember { mutableStateOf(true) }
+    val navBackStackEntry by appState.navController.currentBackStackEntryAsState()
+    val topLevelDestinationRoute = listOf(
+        lectureListRoute,
+        clubRoute,
+        mainPageRoute,
+        postRoute,
+        myPageRoute
+    )
+
+    navBackStackEntry?.destination?.route?.let {
+        isBottomBarVisible.value = topLevelDestinationRoute.contains(it)
+    }
+
+    BitgoeulAndroidTheme { colors, _ ->
+        Scaffold(
+            containerColor = Color.Transparent,
+            contentColor = colors.WHITE,
+            contentWindowInsets = WindowInsets(0, 0, 0, 0),
+            bottomBar = {
+                if (isBottomBarVisible.value) {
+                    BitgoeulBottomBar(
+                        destination = appState.topLevelDestinations,
+                        onNavigateToDestination = appState::navigateToTopLevelDestination,
+                        currentDestination = appState.currentDestination
+                    )
+                }
+            }
+        ) { _ ->
+            BitgoeulNavHost(appState = appState)
+        }
     }
 }
+
+@Composable
+fun BitgoeulBottomBar(
+    destination: List<TopLevelDestination>,
+    onNavigateToDestination: (TopLevelDestination) -> Unit,
+    currentDestination: NavDestination?,
+    modifier: Modifier = Modifier
+) {
+    BitgoeulNavigationBar {
+        destination.forEach { destination ->
+            val selected = currentDestination.isTopLevelDestinationInHierarchy(destination)
+            BitgoeulNavigationBarItem(
+                selected = selected,
+                onClick = { onNavigateToDestination(destination) },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = destination.unselectedIcon),
+                        contentDescription = null
+                    )
+                },
+                selectedIcon = {
+                    Icon(
+                        painter = painterResource(id = destination.unselectedIcon),
+                        contentDescription = null,
+                        tint = BitgoeulColor.P5
+                    )
+                },
+                label = {
+                    Text(
+                        text = destination.iconText,
+                        style = Typography.labelMedium
+                    )
+                }
+            )
+        }
+    }
+}
+
+private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination) =
+    this?.hierarchy?.any {
+        it.route?.contains(destination.name, true) ?: false
+    } ?: false
