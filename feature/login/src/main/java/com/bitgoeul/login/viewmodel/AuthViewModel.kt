a0c647a4e1af62c14da46fd2da3700047e227ac6
Chaejongin12 <s22055@gsm.hs.kr>
2024-05-31 15:50:53 +0900

    ♻️ :: AuthViewModel Login함수 수정

--- feature/login/src/main/java/com/bitgoeul/login/viewmodel/AuthViewModel.kt
+++ feature/login/src/main/java/com/bitgoeul/login/viewmodel/AuthViewModel.kt
@@ -14,6 +14,8 @@ import com.msg.domain.auth.SaveTokenUseCase
 import com.msg.model.remote.model.auth.AuthTokenModel
 import com.msg.model.remote.request.auth.LoginRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,25 +32,23 @@ class AuthViewModel @Inject constructor(
     private val _loginResponse = MutableStateFlow<Event<AuthTokenModel>>(Event.Loading)
     val loginResponse = _loginResponse.asStateFlow()
 
-    private val _email = mutableStateOf("")
-    val email: State<String> = _email
-
-    private val _password = mutableStateOf("")
-    val password: State<String> = _password
-
-    fun login(body: LoginRequest) = viewModelScope.launch {
+    fun login(
+        email: String,
+        password: String
+    ) = viewModelScope.launch {
         loginUseCase(
-            body = body
+            body = LoginRequest(email, password)
         ).onSuccess {
             it.catch { remoteError ->
-                _loginRequest.value = remoteError.errorHandling()
+                _loginResponse.value = remoteError.errorHandling()
             }.collect { response ->
-                _loginRequest.value = Event.Success(data = response)
+                _loginResponse.value = Event.Success(data = response)
             }
         }.onFailure {
-            _loginRequest.value = it.errorHandling()
+            _loginResponse.value = it.errorHandling()
         }
     }
+
     fun saveTokenData(data: AuthTokenModel) = viewModelScope.launch {
         saveTokenUseCase(
             data = data
