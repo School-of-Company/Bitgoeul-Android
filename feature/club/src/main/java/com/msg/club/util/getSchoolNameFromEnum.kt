f9dd2326e8f0bafc9ec3729c0504766cc90e7c93
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 23:53:24 +0900

    ♻️:: Club util getSchoolNameFromEnum 재 import

--- feature/club/src/main/java/com/msg/club/util/getSchoolNameFromEnum.kt
+++ feature/club/src/main/java/com/msg/club/util/getSchoolNameFromEnum.kt
@@ -1,7 +1,7 @@
 package com.msg.club.util
 
 import androidx.compose.runtime.mutableStateListOf
-import com.msg.model.remote.enumdatatype.HighSchool
+import com.msg.model.enumdata.HighSchool
 
 fun Collection<HighSchool>.getSchoolNameFromEnum(): List<String> {
     val schoolList = mutableStateListOf<String>()
