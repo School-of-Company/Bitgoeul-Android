a30fb4e86d4508ce691709a846b95ec7e43f7848
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 20:19:32 +0900

    ♻️:: AdminRepository 매핑 적용

--- core/data/src/main/java/com/msg/data/repository/admin/AdminRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/admin/AdminRepositoryImpl.kt
@@ -1,21 +1,26 @@
 package com.msg.data.repository.admin
 
-import com.msg.model.remote.request.admin.GetUserListRequest
-import com.msg.model.remote.response.admin.UserListResponse
+import com.msg.data.mapper.admin.toEntity
+import com.msg.data.mapper.admin.toRequest
+import com.msg.model.entity.admin.UserListEntity
+import com.msg.model.param.admin.GetUserListParam
 import com.msg.network.datasource.admin.AdminDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 class AdminRepositoryImpl @Inject constructor(
     private val adminDataSource: AdminDataSource,
 ) : AdminRepository {
     override fun getUserList(
-        body: GetUserListRequest,
+        body: GetUserListParam,
         keyword: String,
-    ): Flow<UserListResponse> {
+    ): Flow<UserListEntity> {
         return adminDataSource.getUserList(
-            body = body,
+            body = body.toRequest(),
             keyword = keyword
-        )
+        ).transform { response ->
+            response.toEntity()
+        }
     }
 }
