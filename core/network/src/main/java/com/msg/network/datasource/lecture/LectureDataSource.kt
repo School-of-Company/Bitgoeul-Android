9078521840b2c64b34511865a527d1164b43e7df
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 23:07:48 +0900

    ♻️:: LectureDataSource Response Request 아키텍처 변경으로 인한 재 import

--- core/network/src/main/java/com/msg/network/datasource/lecture/LectureDataSource.kt
+++ core/network/src/main/java/com/msg/network/datasource/lecture/LectureDataSource.kt
@@ -1,15 +1,15 @@
 package com.msg.network.datasource.lecture
 
-import com.msg.model.remote.request.lecture.OpenLectureRequest
-import com.msg.model.remote.response.lecture.DetailLectureResponse
-import com.msg.model.remote.response.lecture.DownloadExcelFileResponse
-import com.msg.model.remote.response.lecture.GetLectureSignUpHistoryResponse
-import com.msg.model.remote.response.lecture.GetTakingLectureStudentListResponse
-import com.msg.model.remote.response.lecture.LectureListResponse
-import com.msg.model.remote.response.lecture.SearchDepartmentResponse
-import com.msg.model.remote.response.lecture.SearchDivisionResponse
-import com.msg.model.remote.response.lecture.SearchLineResponse
-import com.msg.model.remote.response.lecture.SearchProfessorResponse
+import com.msg.network.request.lecture.OpenLectureRequest
+import com.msg.network.response.lecture.DetailLectureResponse
+import com.msg.network.response.lecture.DownloadExcelFileResponse
+import com.msg.network.response.lecture.GetLectureSignUpHistoryResponse
+import com.msg.network.response.lecture.GetTakingLectureStudentListResponse
+import com.msg.network.response.lecture.LectureListResponse
+import com.msg.network.response.lecture.SearchDepartmentResponse
+import com.msg.network.response.lecture.SearchDivisionResponse
+import com.msg.network.response.lecture.SearchLineResponse
+import com.msg.network.response.lecture.SearchProfessorResponse
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
