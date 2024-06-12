16afdf794832f18f663ce8df51bb3b880da2eea4
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 22:56:19 +0900

    ♻️:: admin 기능 UseCase 패키징 변경 및 Entity, Param 변경사항 적용

--- core/domain/src/main/java/com/msg/domain/usecase/admin/GetUserListUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/admin/GetUserListUseCase.kt
@@ -1,13 +1,13 @@
-package com.msg.domain.admin
+package com.msg.domain.usecase.admin
 
 import com.msg.data.repository.admin.AdminRepository
-import com.msg.model.remote.request.admin.GetUserListRequest
+import com.msg.model.param.admin.GetUserListParam
 import javax.inject.Inject
 
 class GetUserListUseCase @Inject constructor(
     private val adminRepository: AdminRepository
 ) {
-    suspend operator fun invoke(body: GetUserListRequest, keyword: String) = kotlin.runCatching {
+    suspend operator fun invoke(body: GetUserListParam, keyword: String) = kotlin.runCatching {
         adminRepository.getUserList(body = body, keyword = keyword)
     }
 }
