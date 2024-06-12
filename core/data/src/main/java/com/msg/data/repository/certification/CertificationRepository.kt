a9247ddcce8379b198adc11cd11362a3435bcff9
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 20:19:49 +0900

    ♻️:: CertificationRepository 매핑 적용

--- core/data/src/main/java/com/msg/data/repository/certification/CertificationRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/certification/CertificationRepository.kt
@@ -1,13 +1,13 @@
 package com.msg.data.repository.certification
 
-import com.msg.model.remote.request.certification.WriteCertificationRequest
-import com.msg.model.remote.response.certification.CertificationListResponse
+import com.msg.model.entity.certification.CertificationListEntity
+import com.msg.model.param.certification.WriteCertificationParam
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface CertificationRepository {
-    fun getCertificationListForTeacher(studentId: UUID): Flow<List<CertificationListResponse>>
-    fun getCertificationListForStudent(): Flow<List<CertificationListResponse>>
-    fun writeCertification(body: WriteCertificationRequest): Flow<Unit>
-    fun editCertification(id: UUID, body: WriteCertificationRequest): Flow<Unit>
+    fun getCertificationListForTeacher(studentId: UUID): Flow<List<CertificationListEntity>>
+    fun getCertificationListForStudent(): Flow<List<CertificationListEntity>>
+    fun writeCertification(body: WriteCertificationParam): Flow<Unit>
+    fun editCertification(id: UUID, body: WriteCertificationParam): Flow<Unit>
 }
--- core/data/src/main/java/com/msg/data/repository/certification/CertificationRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/certification/CertificationRepositoryImpl.kt
@@ -1,37 +1,46 @@
 package com.msg.data.repository.certification
 
-import com.msg.model.remote.request.certification.WriteCertificationRequest
-import com.msg.model.remote.response.certification.CertificationListResponse
+import com.msg.data.mapper.certification.toEntity
+import com.msg.data.mapper.certification.toRequest
+import com.msg.model.entity.certification.CertificationListEntity
+import com.msg.model.param.certification.WriteCertificationParam
 import com.msg.network.datasource.certification.CertificationDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
 import java.util.UUID
 import javax.inject.Inject
 
 class CertificationRepositoryImpl @Inject constructor(
     private val certificationDataSource: CertificationDataSource,
 ) : CertificationRepository {
-    override fun getCertificationListForTeacher(studentId: UUID): Flow<List<CertificationListResponse>> {
-        return certificationDataSource.getCertificationListForTeacher(studentId = studentId)
+    override fun getCertificationListForTeacher(studentId: UUID): Flow<List<CertificationListEntity>> {
+        return certificationDataSource.getCertificationListForTeacher(
+            studentId = studentId
+        ).transform { response ->
+            response.map { it.toEntity() }
+        }
     }
 
-    override fun getCertificationListForStudent(): Flow<List<CertificationListResponse>> {
+    override fun getCertificationListForStudent(): Flow<List<CertificationListEntity>> {
         return certificationDataSource.getCertificationListForStudent()
+            .transform { response ->
+                response.map { it.toEntity() }
+            }
     }
 
-    override fun writeCertification(body: WriteCertificationRequest): Flow<Unit> {
+    override fun writeCertification(body: WriteCertificationParam): Flow<Unit> {
         return certificationDataSource.writeCertification(
-            body = body
+            body = body.toRequest()
         )
     }
 
     override fun editCertification(
         id: UUID,
-        body: WriteCertificationRequest,
+        body: WriteCertificationParam,
     ): Flow<Unit> {
         return certificationDataSource.editCertification(
             id = id,
-            body = body
+            body = body.toRequest()
         )
     }
-
 }
