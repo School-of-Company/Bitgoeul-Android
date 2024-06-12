57e467cfcd62f5805a3aad6b84c0a5645ee46e5e
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-12 23:02:17 +0900

    ♻️:: activity 기능 UseCase 패키징 변경 및 Entity, Param 변경사항 적용

--- core/domain/src/main/java/com/msg/domain/usecase/user/ChangePasswordUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/user/ChangePasswordUseCase.kt
@@ -1,13 +1,13 @@
-package com.msg.domain.activity
+package com.msg.domain.usecase.user
 
 import com.msg.data.repository.user.UserRepository
-import com.msg.model.remote.request.user.ChangePasswordRequest
+import com.msg.model.param.user.ChangePasswordParam
 import javax.inject.Inject
 
 class ChangePasswordUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(body: ChangePasswordRequest) = runCatching {
+    suspend operator fun invoke(body: ChangePasswordParam) = runCatching {
         userRepository.changePassword(body = body)
     }
 }
