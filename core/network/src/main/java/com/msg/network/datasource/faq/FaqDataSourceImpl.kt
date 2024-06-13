56457a04088c86cc19d0e8f132072f2d85d698d1
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-13 13:46:51 +0900

    ♻️:: FaqDataSource makeRequest 함수 적용

--- core/network/src/main/java/com/msg/network/datasource/faq/FaqDataSourceImpl.kt
+++ core/network/src/main/java/com/msg/network/datasource/faq/FaqDataSourceImpl.kt
@@ -3,31 +3,16 @@ package com.msg.network.datasource.faq
 import com.msg.network.api.FaqAPI
 import com.msg.network.request.faq.AddFrequentlyAskedQuestionsRequest
 import com.msg.network.response.faq.GetFrequentlyAskedQuestionDetailResponse
-import com.msg.network.util.BitgoeulApiHandler
-import kotlinx.coroutines.Dispatchers
+import com.msg.network.util.makeRequest
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class FaqDataSourceImpl @Inject constructor(
     private val faqAPI: FaqAPI,
 ) : FaqDataSource {
     override fun addFrequentlyAskedQuestions(body: AddFrequentlyAskedQuestionsRequest): Flow<Unit> =
-        flow {
-            emit(
-                BitgoeulApiHandler<Unit>()
-                    .httpRequest { faqAPI.addFrequentlyAskedQuestions(body = body) }
-                    .sendRequest()
-            )
-        }.flowOn(Dispatchers.IO)
+        makeRequest { faqAPI.addFrequentlyAskedQuestions(body = body) }
 
     override fun getFrequentlyAskedQuestionsList(): Flow<List<GetFrequentlyAskedQuestionDetailResponse>> =
-        flow {
-            emit(
-                BitgoeulApiHandler<List<GetFrequentlyAskedQuestionDetailResponse>>()
-                    .httpRequest { faqAPI.getFrequentlyAskedQuestionsList() }
-                    .sendRequest()
-            )
-        }.flowOn(Dispatchers.IO)
+        makeRequest { faqAPI.getFrequentlyAskedQuestionsList() }
 }
