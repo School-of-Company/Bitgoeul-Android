ca612cf67e58307b23597727b8853657a57e6592
명훈 <s23065@gsm.hs.kr>
2024-06-19 19:56:37 +0900

    :memo: :: Apply Reviewer

--- core/network/src/main/java/com/msg/network/response/activity/GetStudentActivityListResponse.kt
+++ core/network/src/main/java/com/msg/network/response/activity/GetStudentActivityListResponse.kt
@@ -1,35 +1,36 @@
 package com.msg.network.response.activity
 
 import com.msg.model.model.activity.GetStudentActivityModel
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 
 @JsonClass(generateAdapter = true)
-data class GetStudentActivityListResponse (
-    val content: List<GetStudentActivityModel>,
-    val pageable: Pageable,
-    val totalPages: Int,
-    val totalElements: Int,
-    val last: Boolean,
-    val numberOfElements: Int,
-    val number: Int,
-    val sort: Sort,
-    val first: Boolean,
-    val size: Int,
-    val empty: Boolean,
+data class GetStudentActivityListResponse(
+    @Json(name = "content") val content: List<GetStudentActivityModel>,
+    @Json(name = "pageable") val pageable: Pageable,
+    @Json(name = "totalPages") val totalPages: Int,
+    @Json(name = "totalElements") val totalElements: Int,
+    @Json(name = "last") val last: Boolean,
+    @Json(name = "numberOfElements") val numberOfElements: Int,
+    @Json(name = "number") val number: Int,
+    @Json(name = "sort") val sort: Sort,
+    @Json(name = "first") val first: Boolean,
+    @Json(name = "size") val size: Int,
+    @Json(name = "empty") val empty: Boolean,
 ) {
     data class Pageable(
-        val sort: Sort,
-        val pageSize: Int,
-        val pageNumber: Int,
-        val offset: Int,
-        val paged: Boolean,
-        val unpaged: Boolean,
+        @Json(name = "sort") val sort: Sort,
+        @Json(name = "pageSize") val pageSize: Int,
+        @Json(name = "pageNumber") val pageNumber: Int,
+        @Json(name = "offset") val offset: Int,
+        @Json(name = "paged") val paged: Boolean,
+        @Json(name = "unpaged") val unpaged: Boolean,
     )
 
     data class Sort(
-        val unsorted: Boolean,
-        val sorted: Boolean,
-        val empty: Boolean,
+        @Json(name = "unsorted") val unsorted: Boolean,
+        @Json(name = "sorted") val sorted: Boolean,
+        @Json(name = "empty") val empty: Boolean,
     )
 }
