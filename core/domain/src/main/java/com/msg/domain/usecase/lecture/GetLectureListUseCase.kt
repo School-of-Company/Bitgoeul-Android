a6ce60be7e8c8c640238e37c39d35915a6550e2c
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-17 11:56:37 +0900

    🔥:: GetLectureListUseCase invoke 함수 suspend 키워드 삭제

--- core/domain/src/main/java/com/msg/domain/usecase/lecture/GetLectureListUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/lecture/GetLectureListUseCase.kt
@@ -6,8 +6,7 @@ import javax.inject.Inject
 class GetLectureListUseCase @Inject constructor(
     private val lectureRepository: LectureRepository,
 ) {
-    suspend operator fun invoke(page: Int, size: Int, type: String?) =
-        runCatching {
-            lectureRepository.getLectureList(page = page, size = size, type = type)
-        }
+    operator fun invoke(page: Int, size: Int, type: String?) = runCatching {
+        lectureRepository.getLectureList(page = page, size = size, type = type)
+    }
 }
