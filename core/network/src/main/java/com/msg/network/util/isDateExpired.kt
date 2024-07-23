909cdbb52c06f0be18f99c9d2392229a386c8097
명훈 <s23065@gsm.hs.kr>
2024-07-23 20:46:35 +0900

    :sparkles: :: Create isDateExpired

---
+++ core/network/src/main/java/com/msg/network/util/isDateExpired.kt
@@ -0,0 +1,16 @@
+package com.msg.network.util
+
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun String.isDateExpired(): Boolean {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    return kotlin.runCatching {
+        val dateTime = LocalDateTime.parse(this, formatter)
+        val currentTime = LocalDateTime.now(ZoneId.systemDefault())
+        dateTime.isBefore(currentTime)
+    }.getOrElse {
+        true
+    }
+}
