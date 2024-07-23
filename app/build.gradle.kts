a22e5849a21192f58dc0777bb70df2a86b003ed6
명훈 <s23065@gsm.hs.kr>
2024-07-23 20:47:44 +0900

    :memo: :: Add Implementation(network) To app/build.gradle.kts File

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:model"))
     implementation(project(":core:domain"))
+    implementation(project(":core:network"))
     implementation(project(":core:design-system"))
     implementation(project(":feature:login"))
     implementation(project(":feature:sign-up"))
