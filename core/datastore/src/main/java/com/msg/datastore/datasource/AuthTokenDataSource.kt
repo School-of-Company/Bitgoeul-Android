9ed290e6a4df51987da9ea6d188c4254950016b0
명훈 <s23065@gsm.hs.kr>
2024-07-23 20:04:36 +0900

    :memo: :: Add AuthToken Delete

--- core/datastore/src/main/java/com/msg/datastore/datasource/AuthTokenDataSource.kt
+++ core/datastore/src/main/java/com/msg/datastore/datasource/AuthTokenDataSource.kt
@@ -8,12 +8,21 @@ import java.time.LocalDateTime
 interface AuthTokenDataSource {
     fun getAccessToken(): Flow<String>
     fun setAccessToken(accessToken: String): Flow<Unit>
+    fun deleteAccessToken(): Flow<Unit>
+
     fun getAccessTokenExp(): Flow<LocalDateTime>
     fun setAccessTokenExp(accessTokenExp: String): Flow<Unit>
+    fun deleteAccessTokenExp(): Flow<Unit>
+
     fun getRefreshToken(): Flow<String>
     fun setRefreshToken(refreshToken: String): Flow<Unit>
+    fun deleteRefreshToken(): Flow<Unit>
+
     fun getRefreshTokenExp(): Flow<LocalDateTime>
     fun setRefreshTokenExp(refreshTokenExp: String): Flow<Unit>
+    fun deleteRefreshTokenExp(): Flow<Unit>
+
     fun getAuthority(): Flow<Authority>
     fun setAuthority(authority: Authority): Flow<Unit>
+    fun deleteAuthority(): Flow<Unit>
 }
