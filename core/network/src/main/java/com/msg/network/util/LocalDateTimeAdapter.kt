a419fc279af7fb89eef53f2c3d1788247eb211db
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-25 13:35:06 +0900

    ✨:: LocalDateTime 파싱을 위한 Adapter 생성

---
+++ core/network/src/main/java/com/msg/network/util/LocalDateTimeAdapter.kt
@@ -0,0 +1,27 @@
+package com.msg.network.util
+
+import android.util.Log
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
+class LocalDateTimeAdapter {
+    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+
+    @FromJson
+    fun fromJson(json: String): LocalDateTime {
+        return try {
+            Log.e("fromJson parsing block", "실행")
+            LocalDateTime.parse(json, formatter)
+        } catch (e: DateTimeParseException) {
+            LocalDateTime.parse(json, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        }
+    }
+
+    @ToJson
+    fun toJson(value: LocalDateTime): String {
+        return value.format(formatter)
+    }
+}
