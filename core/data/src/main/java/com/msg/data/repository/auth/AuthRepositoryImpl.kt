2da2845f3bb8e6a6afb2fee7487f6927a5bfb329
명훈 <s23065@gsm.hs.kr>
2024-07-25 20:10:02 +0900

    :rewind: :: Add RefreshToken Header

--- core/data/src/main/java/com/msg/data/repository/auth/AuthRepositoryImpl.kt
+++ core/data/src/main/java/com/msg/data/repository/auth/AuthRepositoryImpl.kt
@@ -100,8 +100,8 @@ class AuthRepositoryImpl @Inject constructor(
         return localDataSource.getRefreshToken()
     }
 
-    override fun tokenAccess(): Flow<TokenAccessEntity> {
-        return remoteDataSource.tokenAccess().transform { response ->
+    override fun tokenAccess(refreshToken: String): Flow<TokenAccessEntity> {
+        return remoteDataSource.tokenAccess(refreshToken = refreshToken).transform { response ->
             emit(response.toEntity())
         }
     }
