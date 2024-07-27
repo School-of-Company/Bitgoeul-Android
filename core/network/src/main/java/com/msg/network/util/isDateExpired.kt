53b4d423d02bee4da27eb461cb259f86537e4395
명훈 <s23065@gsm.hs.kr>
2024-07-27 10:42:11 +0900

    :memo: :: Add .SSSSSS

--- core/network/src/main/java/com/msg/network/util/isDateExpired.kt
+++ core/network/src/main/java/com/msg/network/util/isDateExpired.kt
@@ -5,7 +5,7 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 fun String.isDateExpired(): Boolean {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     return kotlin.runCatching {
         val dateTime = LocalDateTime.parse(this, formatter)
         val currentTime = LocalDateTime.now(ZoneId.systemDefault())
