129286d563923b9cd7e871218321f1837adc09d4
명훈 <s23065@gsm.hs.kr>
2024-08-13 15:26:11 +0900

    :recycle: :: Simplify Code(checkRegex)

--- core/design-system/src/main/java/com/msg/design_system/util/checkRegex.kt
+++ core/design-system/src/main/java/com/msg/design_system/util/checkRegex.kt
@@ -1,11 +1,5 @@
 package com.msg.design_system.util
 
-fun String.checkEmailRegex(): Boolean {
-    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
-    return this.matches(emailRegex.toRegex())
-}
+fun String.checkEmailRegex(): Boolean = this.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$".toRegex())
 
-fun String.checkPasswordRegex(): Boolean {
-    val passwordRegex = "^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[@#$%^&+=!?.])(?=\\S+$).{8,24}.$"
-    return this.matches(passwordRegex.toRegex())
-}
+fun String.checkPasswordRegex(): Boolean = this.matches("^(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$".toRegex())
