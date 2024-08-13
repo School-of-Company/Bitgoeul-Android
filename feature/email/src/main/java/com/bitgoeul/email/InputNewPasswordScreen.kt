93aedd150626bc9a4cbc092cf195a91e1bea8060
명훈 <s23065@gsm.hs.kr>
2024-08-13 15:06:10 +0900

    :recycle: :: Refactor Unused CompositionLocalProvider Code

--- feature/email/src/main/java/com/bitgoeul/email/InputNewPasswordScreen.kt
+++ feature/email/src/main/java/com/bitgoeul/email/InputNewPasswordScreen.kt
@@ -1,5 +1,6 @@
 package com.bitgoeul.email
 
+import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -39,11 +40,9 @@ internal fun InputNewPasswordRoute(
     onNextClicked: () -> Unit,
     viewModel: EmailViewModel = hiltViewModel(LocalContext.current as ComponentActivity)
 ) {
-    val focusManager = LocalFocusManager.current
 
     InputNewPasswordScreen(
         onBackClicked = onBackClicked,
-        focusManager = focusManager,
         onNextClicked = { newPassword ->
             viewModel.onNewPassword(newPassword)
             viewModel.findPassword()
@@ -55,105 +54,103 @@ internal fun InputNewPasswordRoute(
 @Composable
 internal fun InputNewPasswordScreen(
     modifier: Modifier = Modifier,
-    focusManager: FocusManager,
+    focusManager: FocusManager = LocalFocusManager.current,
     onBackClicked: () -> Unit,
     onNextClicked: (String) -> Unit,
+    context: Context = LocalContext.current
 ) {
     val passwordPattern = Regex("^(?=.*[A-Za-z0-9])[A-Za-z0-9!@#$%^&*]{8,24}$")
     val firstInputPassword = remember { mutableStateOf("") }
     val secondInputPassword = remember { mutableStateOf("") }
-    val context = LocalContext.current
 
-    CompositionLocalProvider(LocalFocusManager provides focusManager) {
-        BitgoeulAndroidTheme { color, typography ->
-            Surface {
-                Column(
-                    modifier = modifier
-                        .background(color = color.WHITE)
-                        .padding(horizontal = 28.dp)
-                        .fillMaxSize()
-                        .pointerInput(Unit) {
-                            detectTapGestures {
-                                focusManager.clearFocus()
-                            }
+    BitgoeulAndroidTheme { color, typography ->
+        Surface {
+            Column(
+                modifier = modifier
+                    .background(color = color.WHITE)
+                    .padding(horizontal = 28.dp)
+                    .fillMaxSize()
+                    .pointerInput(Unit) {
+                        detectTapGestures {
+                            focusManager.clearFocus()
                         }
-                ) {
-                    Spacer(modifier = modifier.height(20.dp))
-
-                    GoBackTopBar(
-                        icon = { GoBackIcon() },
-                        text = stringResource(id = R.string.go_back)
-                    ) {
-                        onBackClicked()
                     }
+            ) {
+                Spacer(modifier = modifier.height(20.dp))
 
-                    Spacer(modifier = modifier.height(16.dp))
-
-                    Text(
-                        text = "만나서 반가워요!",
-                        style = typography.titleLarge,
-                        color = color.BLACK
-                    )
-
-                    Text(
-                        text = "어디서 오셨나요?",
-                        style = typography.bodySmall,
-                        color = color.G2
-                    )
-
-                    Spacer(modifier = modifier.height(32.dp))
-
-
-                    DefaultTextField(
-                        modifier = modifier.fillMaxWidth(),
-                        onValueChange = { inputPassword ->
-                            firstInputPassword.value = inputPassword
-                        },
-                        errorText = "비밀번호는 8~24자 영문, 숫자, 특수문자 1개 이상이어야 합니다.",
-                        isDisabled = false,
-                        isError = !passwordPattern.matches(firstInputPassword.value) && firstInputPassword.value.isNotEmpty(),
-                        isLinked = false,
-                        isReverseTrailingIcon = false,
-                        onButtonClicked = {},
-                        placeholder = "8~24자 영문, 숫자, 특수문자 1개 이상",
-                        visualTransformationState = true
-                    )
-
-                    Spacer(modifier = modifier.height(16.dp))
-
-                    DefaultTextField(
-                        modifier = modifier.fillMaxWidth(),
-                        onValueChange = { inputPassword ->
-                            secondInputPassword.value = inputPassword
-                        },
-                        errorText = "비밀번호가 일치하지 않습니다.",
-                        isDisabled = false,
-                        isError = firstInputPassword.value != secondInputPassword.value,
-                        isLinked = false,
-                        isReverseTrailingIcon = false,
-                        onButtonClicked = {},
-                        placeholder = "비밀번호 확인",
-                        visualTransformationState = true
-                    )
-
-                    Spacer(modifier = modifier.weight(1f))
-
-                    BitgoeulButton(
-                        modifier = modifier
-                            .padding(bottom = 14.dp)
-                            .fillMaxWidth()
-                            .height(52.dp),
-                        text = "다음으로",
-                        state = if (firstInputPassword.value.isNotEmpty() && secondInputPassword.value.isNotEmpty()) ButtonState.Enable else ButtonState.Disable,
-                        onClicked = {
-                            if (passwordPattern.matches(firstInputPassword.value)) {
-                                onNextClicked(secondInputPassword.value)
-                            } else {
-                                makeToast(context, "비밀번호는 8~24자 영문, 숫자, 특수문자 1개 이상이어야 합니다.")
-                            }
-                        }
-                    )
+                GoBackTopBar(
+                    icon = { GoBackIcon() },
+                    text = stringResource(id = R.string.go_back)
+                ) {
+                    onBackClicked()
                 }
+
+                Spacer(modifier = modifier.height(16.dp))
+
+                Text(
+                    text = "만나서 반가워요!",
+                    style = typography.titleLarge,
+                    color = color.BLACK
+                )
+
+                Text(
+                    text = "어디서 오셨나요?",
+                    style = typography.bodySmall,
+                    color = color.G2
+                )
+
+                Spacer(modifier = modifier.height(32.dp))
+
+
+                DefaultTextField(
+                    modifier = modifier.fillMaxWidth(),
+                    onValueChange = { inputPassword ->
+                        firstInputPassword.value = inputPassword
+                    },
+                    errorText = "비밀번호는 8~24자 영문, 숫자, 특수문자 1개 이상이어야 합니다.",
+                    isDisabled = false,
+                    isError = !passwordPattern.matches(firstInputPassword.value) && firstInputPassword.value.isNotEmpty(),
+                    isLinked = false,
+                    isReverseTrailingIcon = false,
+                    onButtonClicked = {},
+                    placeholder = "8~24자 영문, 숫자, 특수문자 1개 이상",
+                    visualTransformationState = true
+                )
+
+                Spacer(modifier = modifier.height(16.dp))
+
+                DefaultTextField(
+                    modifier = modifier.fillMaxWidth(),
+                    onValueChange = { inputPassword ->
+                        secondInputPassword.value = inputPassword
+                    },
+                    errorText = "비밀번호가 일치하지 않습니다.",
+                    isDisabled = false,
+                    isError = firstInputPassword.value != secondInputPassword.value,
+                    isLinked = false,
+                    isReverseTrailingIcon = false,
+                    onButtonClicked = {},
+                    placeholder = "비밀번호 확인",
+                    visualTransformationState = true
+                )
+
+                Spacer(modifier = modifier.weight(1f))
+
+                BitgoeulButton(
+                    modifier = modifier
+                        .padding(bottom = 14.dp)
+                        .fillMaxWidth()
+                        .height(52.dp),
+                    text = "다음으로",
+                    state = if (firstInputPassword.value.isNotEmpty() && secondInputPassword.value.isNotEmpty()) ButtonState.Enable else ButtonState.Disable,
+                    onClicked = {
+                        if (passwordPattern.matches(firstInputPassword.value)) {
+                            onNextClicked(secondInputPassword.value)
+                        } else {
+                            makeToast(context, "비밀번호는 8~24자 영문, 숫자, 특수문자 1개 이상이어야 합니다.")
+                        }
+                    }
+                )
             }
         }
     }
