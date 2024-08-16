e5bd2dab6160cb774078a4c9063b3e729455bf24
명훈 <s23065@gsm.hs.kr>
2024-08-17 02:37:50 +0900

    :recycle: :: Fire Unused Import && contains() -> in

--- feature/post/src/main/java/com/msg/post/PostDetailScreen.kt
+++ feature/post/src/main/java/com/msg/post/PostDetailScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- feature/post/src/main/java/com/msg/post/PostScreen.kt
+++ feature/post/src/main/java/com/msg/post/PostScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -144,7 +143,7 @@ internal fun PostScreen(
                     onClick = {},
                     content = { HelpIcon() }
                 )
-                if (roleField.contains(role)) {
+                if (role in roleField) {
                     IconButton(
                         modifier = modifier.padding(end = 28.dp),
                         onClick = { onAddClicked(viewState) },
