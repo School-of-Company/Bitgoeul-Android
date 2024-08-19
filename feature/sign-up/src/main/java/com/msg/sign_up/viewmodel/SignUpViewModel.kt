89172bbcf15c1fa73c4ba0cb8f9910ebfa3e9a16
명훈 <s23065@gsm.hs.kr>
2024-08-19 15:53:12 +0900

    :recycle: :: Use SavedStateHandle To SignUpViewModel

--- feature/sign-up/src/main/java/com/msg/sign_up/viewmodel/SignUpViewModel.kt
+++ feature/sign-up/src/main/java/com/msg/sign_up/viewmodel/SignUpViewModel.kt
@@ -2,6 +2,7 @@ package com.msg.sign_up.viewmodel
 
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.common.errorhandling.errorHandling
@@ -33,40 +34,56 @@ class SignUpViewModel @Inject constructor(
     private val signUpBbozzakTeacherUseCase: SignUpBbozzakTeacherUseCase,
     private val signUpProfessorUseCase: SignUpProfessorUseCase,
     private val signUpGovernmentUseCase: SignUpGovernmentUseCase,
-    private val signUpCompanyInstructorUseCase: SignUpCompanyInstructorUseCase
+    private val signUpCompanyInstructorUseCase: SignUpCompanyInstructorUseCase,
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+    companion object {
+        private const val JOB = "job"
+        private const val SCHOOL = "school"
+        private const val CLUB = "club"
+        private const val NAME = "name"
+        private const val PHONE_NUMBER = "phoneNumber"
+        private const val COLLEGE = "college"
+        private const val ENROLLMENT = "enrollment"
+        private const val ENTERPRISE = "enterprise"
+        private const val GOVERNMENT = "government"
+        private const val GRADE_AND_NUMBER = "gradeAndNumber"
+        private const val POSITION = "position"
+        private const val SECTORS = "sectors"
+        private const val EMAIL = "email"
+        private const val PASSWORD = "password"
+    }
 
     private val _signUpResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
     val signUpResponse = _signUpResponse.asStateFlow()
 
-    var job = mutableStateOf("")
-        private set
-    var school = mutableStateOf("")
-        private set
-    var club = mutableStateOf("")
-        private set
-    var name = mutableStateOf("")
-        private set
-    var phoneNumber = mutableStateOf("")
-        private set
-    var college = mutableStateOf("")
-        private set
-    var enrollment = mutableIntStateOf(0)
-        private set
-    var enterprise = mutableStateOf("")
-        private set
-    var government = mutableStateOf("")
-        private set
-    var gradeAndNumber = mutableStateOf("")
-        private set
-    var position = mutableStateOf("")
-        private set
-    var sectors = mutableStateOf("")
-        private set
-    var email = mutableStateOf("")
-        private set
-    var password = mutableStateOf("")
-        private set
+    internal var job = savedStateHandle.getStateFlow(key = JOB, initialValue = "")
+
+    internal var school = savedStateHandle.getStateFlow(key = SCHOOL, initialValue = "")
+
+    internal var club = savedStateHandle.getStateFlow(key = CLUB, initialValue = "")
+
+    internal var name = savedStateHandle.getStateFlow(key = NAME, initialValue = "")
+
+    internal var phoneNumber = savedStateHandle.getStateFlow(key = PHONE_NUMBER, initialValue = "")
+
+    internal var college = savedStateHandle.getStateFlow(key = COLLEGE, initialValue = "")
+
+    internal var enrollment = savedStateHandle.getStateFlow(key = ENROLLMENT, initialValue = 0)
+
+    internal var enterprise = savedStateHandle.getStateFlow(key = ENTERPRISE, initialValue = "")
+
+    internal var government = savedStateHandle.getStateFlow(key = GOVERNMENT, initialValue = "")
+
+    internal var gradeAndNumber = savedStateHandle.getStateFlow(key = GRADE_AND_NUMBER, initialValue = "")
+
+    internal var position = savedStateHandle.getStateFlow(key = POSITION, initialValue = "")
+
+    internal var sectors = savedStateHandle.getStateFlow(key = SECTORS, initialValue = "")
+
+    internal var email = savedStateHandle.getStateFlow(key = EMAIL, initialValue = "")
+
+    internal var password = savedStateHandle.getStateFlow(key = PASSWORD, initialValue = "")
 
     fun signUp() = viewModelScope.launch {
         when (job.value) {
@@ -82,7 +99,7 @@ class SignUpViewModel @Inject constructor(
                         grade = gradeAndNumber.value.substring(0, 1).toInt(),
                         classRoom = gradeAndNumber.value.substring(1, 2).toInt(),
                         number = gradeAndNumber.value.substring(2).toInt(),
-                        admissionNumber = enrollment.intValue
+                        admissionNumber = enrollment.value
                     )
                 ).onSuccess {
                     it.catch { remoteError ->
@@ -202,4 +219,31 @@ class SignUpViewModel @Inject constructor(
         }
     }
 
+    internal fun onJobChange(value: String) { savedStateHandle[JOB] = value }
+
+    internal fun onSchoolChange(value: String) { savedStateHandle[SCHOOL] = value }
+
+    internal fun onClubChange(value: String) { savedStateHandle[CLUB] = value }
+
+    internal fun onNameChange(value: String) { savedStateHandle[NAME] = value }
+
+    internal fun onPhoneNumberChange(value: String) { savedStateHandle[PHONE_NUMBER] = value }
+
+    internal fun onCollegeChange(value: String) { savedStateHandle[COLLEGE] = value }
+
+    internal fun onEnrollmentChange(value: Int) { savedStateHandle[ENROLLMENT] = value }
+
+    internal fun onEnterpriseChange(value: String) { savedStateHandle[ENTERPRISE] = value }
+
+    internal fun onGovernmentChange(value: String) { savedStateHandle[GOVERNMENT] = value }
+
+    internal fun onGradeAndNumberChange(value: String) { savedStateHandle[GRADE_AND_NUMBER] = value }
+
+    internal fun onPositionChange(value: String) { savedStateHandle[POSITION] = value }
+
+    internal fun onSectorsChange(value: String) { savedStateHandle[SECTORS] = value }
+
+    internal fun onEmailChange(value: String) { savedStateHandle[EMAIL] = value }
+
+    internal fun onPasswordChange(value: String) { savedStateHandle[PASSWORD] = value }
 }
