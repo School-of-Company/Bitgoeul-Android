77416d9b1618328e1aa5f301e283d4e9b5e68d3b
명훈 <s23065@gsm.hs.kr>
2024-08-20 17:47:15 +0900

    :recycle: :: Refactor AddActivityScreen

--- feature/student-activity/src/main/java/com/msg/student_activity/AddActivityScreen.kt
+++ feature/student-activity/src/main/java/com/msg/student_activity/AddActivityScreen.kt
@@ -1,5 +1,7 @@
 package com.msg.student_activity
 
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
@@ -15,15 +17,18 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.msg.design_system.component.button.BitgoeulButton
 import com.msg.design_system.component.button.ButtonState
 import com.msg.design_system.component.button.DetailSettingButton
@@ -36,13 +41,19 @@ import java.time.LocalDate
 
 @Composable
 fun AddActivityRoute(
+    viewModel: StudentActivityViewModel = hiltViewModel(LocalContext.current as ComponentActivity),
     onActionClicked: () -> Unit,
     onSettingClicked: () -> Unit,
-    onBackClicked: () -> Unit,
-    viewModel: StudentActivityViewModel = hiltViewModel()
+    onBackClicked: () -> Unit
 ) {
+    val titleValue by viewModel.title.collectAsStateWithLifecycle()
+    val contentValue by viewModel.content.collectAsStateWithLifecycle()
 
     AddActivityScreen(
+        title = titleValue,
+        content = contentValue,
+        onTitleChange = viewModel::onTitleChange,
+        onContentChange = viewModel::onContentChange,
         onActionClicked = {
             onActionClicked()
             viewModel.addActivityInfo(
@@ -53,13 +64,11 @@ fun AddActivityRoute(
             )
         },
         onSettingClicked = { title, content ->
-            viewModel.title.value = title
-            viewModel.content.value = content
+            viewModel.onTitleChange(title)
+            viewModel.onContentChange(content)
             onSettingClicked()
         },
         onBackClicked = onBackClicked,
-        savedTitle = viewModel.title.value,
-        savedContent = viewModel.content.value,
         detailState = viewModel.detailState.value
     )
 }
@@ -68,23 +77,19 @@ fun AddActivityRoute(
 fun AddActivityScreen(
     modifier: Modifier = Modifier,
     focusManager: FocusManager = LocalFocusManager.current,
+    scrollState: ScrollState = rememberScrollState(),
+    maxTitleLength: Int = 100,
+    maxContentLength: Int = 1000,
+    title: String,
+    content: String,
+    onTitleChange: (String) -> Unit,
+    onContentChange: (String) -> Unit,
     onActionClicked: () -> Unit,
     onSettingClicked: (title: String, content: String) -> Unit,
     onBackClicked: () -> Unit,
-    savedTitle: String,
-    savedContent: String,
     detailState: Boolean
 ) {
-
-    val title = remember { mutableStateOf(savedTitle) }
-    val content = remember { mutableStateOf(savedContent) }
-
-    val isDialogVisible = remember { mutableStateOf(false) }
-
-    val maxTitleLength = 100
-    val maxContentLength = 1000
-
-    val scrollState = rememberScrollState()
+    val (isDialogVisible, isSetDialogVisible) = rememberSaveable { mutableStateOf(false) }
 
     BitgoeulAndroidTheme { colors, typography ->
         Surface(
@@ -101,7 +106,7 @@ fun AddActivityScreen(
                     .fillMaxSize()
                     .background(color = colors.WHITE)
             ) {
-                Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = modifier.height(20.dp))
                 GoBackTopBar(
                     icon = { GoBackIcon() },
                     text = "돌아가기"
@@ -116,13 +121,12 @@ fun AddActivityScreen(
                         .weight(1f)
                 ) {
                     BasicTextField(
-                        modifier = modifier
-                            .fillMaxWidth(),
-                        value = title.value,
-                        onValueChange = { if (it.length <= maxTitleLength) title.value = it },
+                        modifier = modifier.fillMaxWidth(),
+                        value = title,
+                        onValueChange = { if (it.length <= maxTitleLength) onTitleChange(it) },
                         textStyle = typography.titleSmall,
                         decorationBox = { innerTextField ->
-                            if (title.value.isEmpty()) Text(
+                            if (title.isEmpty()) Text(
                                 text = "활동 제목 (100자 이내)",
                                 style = typography.titleSmall,
                                 color = colors.G1
@@ -139,13 +143,11 @@ fun AddActivityScreen(
                     Spacer(modifier = modifier.height(16.dp))
                     BasicTextField(
                         modifier = modifier.fillMaxWidth(),
-                        value = content.value,
-                        onValueChange = {
-                            if (it.length <= maxContentLength) content.value = it
-                        },
+                        value = content,
+                        onValueChange = { if (it.length <= maxContentLength) onContentChange(it) },
                         textStyle = typography.bodySmall,
                         decorationBox = { innerTextField ->
-                            if (content.value.isEmpty()) Text(
+                            if (content.isEmpty()) Text(
                                 text = "본문 입력 (1000자 이내)",
                                 style = typography.bodySmall,
                                 color = colors.G1
@@ -169,24 +171,24 @@ fun AddActivityScreen(
                         modifier = modifier.fillMaxWidth(),
                         type = "활동"
                     ) {
-                        onSettingClicked(title.value, content.value)
+                        onSettingClicked(title, content)
                     }
                     Spacer(modifier = modifier.height(8.dp))
                     BitgoeulButton(
                         modifier = modifier.fillMaxWidth(),
                         text = "활동 추가",
-                        state = if (title.value.isNotEmpty() && content.value.isNotEmpty() && detailState) ButtonState.Enable else ButtonState.Disable
+                        state = if (title.isNotEmpty() && content.isNotEmpty() && detailState) ButtonState.Enable else ButtonState.Disable
                     ) {
-                        isDialogVisible.value = true
+                        isSetDialogVisible(true)
                     }
                     Spacer(modifier = modifier.height(16.dp))
                 }
                 PositiveActionDialog(
                     title = "활동 추가하시겠습니까?",
                     positiveAction = "신청",
-                    content = title.value,
-                    isVisible = isDialogVisible.value,
-                    onQuit = { isDialogVisible.value = false },
+                    content = title,
+                    isVisible = isDialogVisible,
+                    onQuit = { isSetDialogVisible(false) },
                     onActionClicked = onActionClicked
                 )
             }
@@ -200,10 +202,12 @@ fun AddActivityScreenPre() {
     AddActivityScreen(
         onActionClicked = {},
         onSettingClicked = { _, _ -> },
-        savedTitle = "",
-        savedContent = "",
         detailState = false,
         onBackClicked = {},
-        focusManager = LocalFocusManager.current
+        focusManager = LocalFocusManager.current,
+        title = "",
+        content = "",
+        onTitleChange = {},
+        onContentChange = {}
     )
 }
