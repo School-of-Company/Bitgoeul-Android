f33bac8d4c9ef9c7c1046b14553b0ce514019f61
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-08-21 13:49:16 +0900

    :memo: add Field Enum

---
+++ core/model/src/main/java/com/msg/model/enumdata/Field.kt
@@ -0,0 +1,9 @@
+package com.msg.model.enumdata
+
+enum class Field(val text: String) {
+    FUTURISTIC_TRANSPORTATION_EQUIPMENT("미래형 운송기기"),
+    ENERGY("에너지 산업"),
+    MEDICAL_HEALTHCARE("의료•헬스케어"),
+    AI_CONVERGENCE("AI 융•복합"),
+    CULTURE("문화산업")
+}
