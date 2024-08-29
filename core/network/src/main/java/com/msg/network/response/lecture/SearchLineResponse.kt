fc1320a8c15ad586453d4b4d8322d9a55a33e986
Chaejongin12 <s22055@gsm.hs.kr>
2024-08-29 19:54:35 +0900

    🔥:: Search API Response 삭제

--- core/network/src/main/java/com/msg/network/response/lecture/SearchLineResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.msg.network.response.lecture
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class SearchLineResponse(
-    @Json(name = "lines") val lines: List<String>,
-)
