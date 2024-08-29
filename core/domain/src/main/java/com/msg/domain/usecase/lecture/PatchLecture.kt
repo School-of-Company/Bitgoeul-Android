4a3af7a284d8b63e1a94d1965ac3fe7c94cb5a8e
Chaejongin12 <s22055@gsm.hs.kr>
2024-08-29 20:20:36 +0900

    :green_heart: :: PatchLectureUseCase 주석처리
    :: PatchLectureUseCase 주석처리

--- core/domain/src/main/java/com/msg/domain/usecase/lecture/PatchLecture.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/lecture/PatchLecture.kt
@@ -1,14 +1,14 @@
 package com.msg.domain.usecase.lecture
 
-import com.msg.data.repository.lecture.LectureRepository
-import com.msg.model.param.lecture.OpenLectureParam
-import java.util.UUID
-import javax.inject.Inject
-
-class PatchLecture @Inject constructor(
-    private val lectureRepository: LectureRepository
-) {
-    operator fun invoke(id: UUID, body: OpenLectureParam) = runCatching {
-        lectureRepository.patchLecture(id = id, body = body)
-    }
-}
+//import com.msg.data.repository.lecture.LectureRepository
+//import com.msg.model.param.lecture.OpenLectureParam
+//import java.util.UUID
+//import javax.inject.Inject
+//
+//class PatchLecture @Inject constructor(
+//    private val lectureRepository: LectureRepository
+//) {
+//    operator fun invoke(id: UUID, body: OpenLectureParam) = runCatching {
+//        lectureRepository.patchLecture(id = id, body = body)
+//    }
+//}
