9d2546d642870721ced13d39f09cfa28565f5158
Chaejongin12 <s22055@gsm.hs.kr>
2024-08-30 14:23:22 +0900

    🔥:: Repository 게시글 생성 함수 삭제

--- core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
+++ core/data/src/main/java/com/msg/data/repository/post/PostRepository.kt
@@ -3,12 +3,10 @@ package com.msg.data.repository.post
 import com.msg.model.entity.post.GetDetailPostEntity
 import com.msg.model.entity.post.GetPostListEntity
 import com.msg.model.enumdata.FeedType
-import com.msg.model.param.post.WritePostParam
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface PostRepository {
-    fun sendPost(body: WritePostParam): Flow<Unit>
     fun getPostList(type: FeedType, size: Int, page: Int): Flow<GetPostListEntity>
     fun getDetailPost(id: UUID): Flow<GetDetailPostEntity>
     fun editPost(id: UUID, body: WritePostParam): Flow<Unit>
