532eac01026fb52b8042c1b556eea12ed414e37b
명훈 <s23065@gsm.hs.kr>
2024-09-05 20:09:10 +0900

    :fire: :: Fire Unused Import

--- feature/certification/src/main/java/com/msg/certification/AddCertificationScreen.kt
+++ feature/certification/src/main/java/com/msg/certification/AddCertificationScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
