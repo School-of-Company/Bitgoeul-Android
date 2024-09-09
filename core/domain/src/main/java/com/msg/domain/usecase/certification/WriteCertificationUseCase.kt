a31bd098a4ba179bb3b8b168c4c90a563edde89c
명훈 <s23065@gsm.hs.kr>
2024-09-09 16:19:04 +0900

    :recycle: :: Use Flow To WriteCertificationUseCase

--- core/domain/src/main/java/com/msg/domain/usecase/certification/WriteCertificationUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/certification/WriteCertificationUseCase.kt
@@ -2,12 +2,12 @@ package com.msg.domain.usecase.certification
 
 import com.msg.data.repository.certification.CertificationRepository
 import com.msg.model.param.certification.WriteCertificationParam
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class WriteCertificationUseCase @Inject constructor(
     private val certificationRepository: CertificationRepository
 ) {
-    suspend operator fun invoke(body: WriteCertificationParam) = runCatching {
+    operator fun invoke(body: WriteCertificationParam): Flow<Unit> =
         certificationRepository.writeCertification(body = body)
-    }
 }
