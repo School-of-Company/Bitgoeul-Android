95b923d20521a8155b73494d72c702d2d921fddd
명훈 <s23065@gsm.hs.kr>
2024-09-09 23:01:39 +0900

    :fire: :: Fire Unused State(Loading) - WriteCertificationUiState

--- feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/WriteCertificationUiState.kt
+++ feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/WriteCertificationUiState.kt
@@ -2,6 +2,5 @@ package com.msg.certification.viewmodel.uistate
 
 sealed interface WriteCertificationUiState {
     data object Success: WriteCertificationUiState
-    data object Loading: WriteCertificationUiState
     data class Error(val exception: Throwable): WriteCertificationUiState
 }
