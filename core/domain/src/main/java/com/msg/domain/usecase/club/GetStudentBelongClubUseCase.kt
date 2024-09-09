0b11882eaa48e5e1a3203e2027f79beddf1f56ce
명훈 <s23065@gsm.hs.kr>
2024-09-09 22:55:55 +0900

    :recycle: :: Use Coroutine Flow(GetStudentBelongClubUseCase)

--- core/domain/src/main/java/com/msg/domain/usecase/club/GetStudentBelongClubUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/club/GetStudentBelongClubUseCase.kt
@@ -1,13 +1,14 @@
 package com.msg.domain.usecase.club
 
 import com.msg.data.repository.club.ClubRepository
+import com.msg.model.entity.club.StudentBelongClubEntity
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 import javax.inject.Inject
 
 class GetStudentBelongClubUseCase @Inject constructor(
     private val clubRepository: ClubRepository
 ) {
-    suspend operator fun invoke(id: Long, studentId: UUID) = runCatching {
+    operator fun invoke(id: Long, studentId: UUID): Flow<StudentBelongClubEntity> =
         clubRepository.getStudentBelongClubDetail(id = id, studentId = studentId)
-    }
 }
