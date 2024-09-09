ef2d7ea59034d4a1ffe674d3a75fea5fa5c8a911
명훈 <s23065@gsm.hs.kr>
2024-09-09 22:59:41 +0900

    :recycle: :: Use data class State Error - GetLectureSignUpHistoryUiState

--- feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/GetLectureSignUpHistoryUiState.kt
+++ feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/GetLectureSignUpHistoryUiState.kt
@@ -5,5 +5,5 @@ import com.msg.model.entity.lecture.GetLectureSignUpHistoryEntity
 sealed interface GetLectureSignUpHistoryUiState {
     data class Success(val data: GetLectureSignUpHistoryEntity): GetLectureSignUpHistoryUiState
     data object Loading: GetLectureSignUpHistoryUiState
-    data object Error: GetLectureSignUpHistoryUiState
+    data class Error(val expectation: Throwable?): GetLectureSignUpHistoryUiState
 }
