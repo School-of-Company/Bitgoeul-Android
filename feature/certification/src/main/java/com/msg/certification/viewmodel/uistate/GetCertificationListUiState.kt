a0f73c3ad6d9b126e033992b1831bcac8bdeb1a1
명훈 <s23065@gsm.hs.kr>
2024-09-09 22:59:08 +0900

    :recycle: :: Use data class State Error - GetCertificationListUiState

--- feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/GetCertificationListUiState.kt
+++ feature/certification/src/main/java/com/msg/certification/viewmodel/uistate/GetCertificationListUiState.kt
@@ -4,7 +4,7 @@ import com.msg.model.entity.certification.CertificationListEntity
 
 sealed interface GetCertificationListUiState {
     data class Success(val data: List<CertificationListEntity>): GetCertificationListUiState
-    data object Error: GetCertificationListUiState
+    data class Error(val expectation: Throwable): GetCertificationListUiState
     data object Loading: GetCertificationListUiState
     data object Empty: GetCertificationListUiState
 }
