6a21b779a869c4b9ae5a2f81a2073338881d0848
명훈 <s23065@gsm.hs.kr>
2024-09-09 11:41:35 +0900

    :memo: :: Modify Use Flow And Fire Unused Code

--- core/domain/src/main/java/com/msg/domain/usecase/lecture/GetLectureSignUpHistoryUseCase.kt
+++ core/domain/src/main/java/com/msg/domain/usecase/lecture/GetLectureSignUpHistoryUseCase.kt
@@ -1,13 +1,14 @@
 package com.msg.domain.usecase.lecture
 
 import com.msg.data.repository.lecture.LectureRepository
+import com.msg.model.entity.lecture.GetLectureSignUpHistoryEntity
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 import javax.inject.Inject
 
 class GetLectureSignUpHistoryUseCase @Inject constructor(
     private val lectureRepository: LectureRepository
 ) {
-    suspend operator fun invoke(studentId: UUID) = runCatching {
+    operator fun invoke(studentId: UUID): Flow<GetLectureSignUpHistoryEntity> =
         lectureRepository.getLectureSignUpHistory(studentId = studentId)
-    }
 }
