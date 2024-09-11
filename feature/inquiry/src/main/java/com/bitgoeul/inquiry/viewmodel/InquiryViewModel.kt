82e7fab200bc94451f9cb52d98e5590686495c00
Chaejongin12 <s22055@gsm.hs.kr>
2024-09-11 10:24:33 +0900

    ✨:: InquiryViewModel 세

---
+++ feature/inquiry/src/main/java/com/bitgoeul/inquiry/viewmodel/InquiryViewModel.kt
@@ -0,0 +1,10 @@
+package com.bitgoeul.inquiry.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InquiryViewModel @Inject constructor() : ViewModel() {
+
+}
